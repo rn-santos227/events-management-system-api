b55c76771fc9da89427afbc0a08a98645adefba1
rn-santos227 <rn_santos227@yahoo.com>
2025-10-28 19:15:41 +0800

    changed yourorg to mgnt

--- src/main/java/com/mgnt/events/EventsApiApplication.java
+++ src/main/java/com/mgnt/events/EventsApiApplication.java
@@ -1,4 +1,4 @@
-package com.yourorg.events;
+package com.mgnt.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/mgnt/events/EventsApiApplicationTests.java
+++ src/test/java/com/mgnt/events/EventsApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yourorg.events;
+package com.mgnt.events;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
