eec9734fa0280c9486202824676bab964bb12f0a
rn-santos227 <rn_santos227@yahoo.com>
2025-10-29 21:20:14 +0800

    updated role model file

--- src/main/java/com/mgnt/events/models/Role.java
+++ src/main/java/com/mgnt/events/models/Role.java
@@ -2,10 +2,16 @@ package com.mgnt.events.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 import java.util.Set;
 
 @Entity
 @Table(name = "roles")
 public class Role {
-    
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 }
