5e6d5ee0f44d27e7a6d6384ee90f2ff5002f410a
rn-santos227 <rn_santos227@yahoo.com>
2025-10-29 22:18:23 +0800

    updated test controller file

--- src/main/java/com/mgnt/events/web/TestController.java
+++ src/main/java/com/mgnt/events/web/TestController.java
@@ -4,9 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
+import com.mgnt.events.constants.Routes;
+
 @RestController
 public class TestController {
-  @GetMapping("/test")
+  @GetMapping(Routes.TEST)
   public Map<String, String> test() {
     return Map.of("message", "this is a test");
   }  
