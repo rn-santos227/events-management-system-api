f3ca66a56389377238acba21e4e8898e102174b3
rn-santos227 <rn_santos227@yahoo.com>
2025-10-29 21:21:01 +0800

    updated role model file

--- src/main/java/com/mgnt/events/models/Role.java
+++ src/main/java/com/mgnt/events/models/Role.java
@@ -1,8 +1,12 @@
 package com.mgnt.events.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
@@ -14,4 +18,27 @@ public class Role {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(unique = true, nullable = false)
+  private String name;
+
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(
+    name = "roles_privileges",
+    joinColumns = @JoinColumn(name = "role_id"),
+    inverseJoinColumns = @JoinColumn(name = "privilege_id")
+  )
+  private Set<Privilege> privileges;
+
+  @ManyToMany(mappedBy = "roles")
+  private Set<User> users;
+
+  public Role() {}
+  public Role(String name) { this.name = name; }
+
+  public Long getId() { return id; }
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
+  public Set<Privilege> getPrivileges() { return privileges; }
+  public void setPrivileges(Set<Privilege> privileges) { this.privileges = privileges; }
 }
