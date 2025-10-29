421b6da2e3ad3592a8cbf3e08cae502a822bac98
rn-santos227 <rn_santos227@yahoo.com>
2025-10-29 22:09:39 +0800

    created joins constant file

---
+++ src/main/java/com/mgnt/events/libraries/Joins.java
@@ -0,0 +1,7 @@
+package com.mgnt.events.libraries;
+
+public class Joins {
+  private Joins() {}
+  public static final String ROLES_PRIVELEGES = "roles_privileges";
+  public static final String USERS_ROLES = "users_roles";
+}
