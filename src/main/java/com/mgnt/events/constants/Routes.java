7266181a52b60ce35942cd2efbae1d046708ba78
rn-santos227 <rn_santos227@yahoo.com>
2025-10-29 22:17:30 +0800

    created routes constant file

---
+++ src/main/java/com/mgnt/events/constants/Routes.java
@@ -0,0 +1,11 @@
+package com.mgnt.events.constants;
+
+public class Routes {
+  private Routes() {}
+
+  public static final String API_BASE = "/api";
+  public static final String API_VERSION = "/v1";
+
+  public static final String BASE_PATH = API_BASE + API_VERSION;
+  public static final String TEST = BASE_PATH + "/test";
+}
