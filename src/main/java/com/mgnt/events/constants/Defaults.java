a5f421597b9fb57da50e073e1e39bac0243ce4ac
rn-santos227 <rn_santos227@yahoo.com>
2025-10-29 22:30:51 +0800

    updated user role file

---
+++ src/main/java/com/mgnt/events/constants/Defaults.java
@@ -0,0 +1,8 @@
+package com.mgnt.events.constants;
+
+public class Defaults {
+  Defaults() {}
+
+  public static final int DEFAULT_EMAIL_LENGTH = 100;
+  public static final int DEFAULT_MID_STRING_LENGTH = 50;
+}
--- src/main/java/com/mgnt/events/models/User.java
+++ src/main/java/com/mgnt/events/models/User.java
@@ -1,25 +1,39 @@
 package com.mgnt.events.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.mgnt.events.constants.Defaults;
+import com.mgnt.events.constants.Tables;
 
 @Entity
-@Table(name = "users")
+@Table(name = Tables.USERS)
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false, unique = true, length = 100)
+  @Column(nullable = false, unique = true, length = Defaults.DEFAULT_EMAIL_LENGTH)
   private String email;
 
   @Column(nullable = false)
   private String password;
 
-  @Column(nullable = false, length = 50)
+  @Column(nullable = false, length = Defaults.DEFAULT_MID_STRING_LENGTH)
   private String firstName;
 
-  @Column(nullable = false, length = 50)
+  @Column(nullable = false, length = Defaults.DEFAULT_MID_STRING_LENGTH)
   private String lastName;
 
   @Column(length = 20)
