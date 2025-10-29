2fa3badae7c1c324c535930d44383f29bc3e6a5e
rn-santos227 <rn_santos227@yahoo.com>
2025-10-29 21:19:14 +0800

    created role model file

---
+++ src/main/java/com/mgnt/events/models/Role.java
@@ -0,0 +1,11 @@
+package com.mgnt.events.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    
+}
