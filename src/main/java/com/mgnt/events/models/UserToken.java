ccd893b5d1a2a70e2f595dc219ae5ea81f8da4a1
rn-santos227 <rn_santos227@yahoo.com>
2025-10-30 20:21:24 +0800

    added token and token type fields in user token model file

--- src/main/java/com/mgnt/events/models/UserToken.java
+++ src/main/java/com/mgnt/events/models/UserToken.java
@@ -1,12 +1,18 @@
 package com.mgnt.events.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import com.mgnt.events.constants.Attributes;
+import com.mgnt.events.constants.Defaults;
 import com.mgnt.events.constants.Tables;
+import com.mgnt.events.types.TokenType;
 
 @Entity
 @Table(name = Tables.USER_TOKENS)
@@ -14,4 +20,11 @@ public class UserToken {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(nullable = false, unique = false, length = Defaults.DEFAULT_TOKEN_LENGTH)
+  private String token;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = Attributes.TOKEN_TYPE, nullable = false)
+  private TokenType tokenType = TokenType.BEARER;
 }
