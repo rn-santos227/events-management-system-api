b6c4d299b00bcdb51cffcd3f406ac9bc4e61e380
rn-santos227 <rn_santos227@yahoo.com>
2025-10-30 14:37:39 +0800

    updated attributes and defaults constants files

--- src/main/java/com/mgnt/events/constants/Attributes.java
+++ src/main/java/com/mgnt/events/constants/Attributes.java
@@ -6,9 +6,13 @@ public class Attributes {
   public static final String ROLE_ID = "role_id";
   public static final String PRIVILEGE_ID = "privilege_id";
 
+  public static final String CONTACT_NUMBER = "contact_number";
   public static final String FIRST_NAME = "first_name";
   public static final String LAST_NAME = "last_name";
   public static final String ROLE = "role";
+
+  public static final String TOKEN_TYPE = "token_type";
+  public static final String EXPIRES_AT = "expires_at";
 
   public static final String CREATED_AT = "created_at";
   public static final String DELETED_AT = "deleted_at";
--- src/main/java/com/mgnt/events/constants/Defaults.java
+++ src/main/java/com/mgnt/events/constants/Defaults.java
@@ -5,4 +5,6 @@ public class Defaults {
 
   public static final int DEFAULT_EMAIL_LENGTH = 100;
   public static final int DEFAULT_MID_STRING_LENGTH = 50;
+  public static final int DEFAULT_PHONE_LENGTH = 20;
+  public static final int DEFAULT_TOKEN_LENGTH = 512;
 }
