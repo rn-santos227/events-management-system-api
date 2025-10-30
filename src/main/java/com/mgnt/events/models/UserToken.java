acc2f4d12667da2754e7a797166ff1de0389d78b
rn-santos227 <rn_santos227@yahoo.com>
2025-10-30 20:26:03 +0800

    added user fields in user token model file

--- src/main/java/com/mgnt/events/models/UserToken.java
+++ src/main/java/com/mgnt/events/models/UserToken.java
@@ -4,9 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
@@ -38,4 +41,8 @@ public class UserToken {
 
   @Column(name = Attributes.EXPIRES_AT)
   private LocalDateTime expiresAt;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = Attributes.USER_ID, nullable = false)
+  private User user;
 }
