37201896b9478328342f95afe8e395429e1a6425
rn-santos227 <rn_santos227@yahoo.com>
2025-10-31 23:48:49 +0800

    added password encoder function in security config file

--- src/main/java/com/mgnt/events/security/SecurityConfig.java
+++ src/main/java/com/mgnt/events/security/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.mgnt.events.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.mgnt.events.security.auth.JwtAuthenticationFilter;
 
@@ -20,5 +23,10 @@ public class SecurityConfig {
   ) {
     this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     this.userDetailsService = userDetailsService;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
