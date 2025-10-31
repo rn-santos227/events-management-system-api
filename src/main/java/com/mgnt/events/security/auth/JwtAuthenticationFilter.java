42053640fcf956a64e6873a3543717c2e0690bef
rn-santos227 <rn_santos227@yahoo.com>
2025-10-31 21:34:47 +0800

    added constructor in jwt authentication filter file

--- src/main/java/com/mgnt/events/security/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/mgnt/events/security/auth/JwtAuthenticationFilter.java
@@ -1,15 +1,33 @@
 package com.mgnt.events.security.auth;
 
-import java.io.IOException;
-
-import org.springframework.web.filter.OncePerRequestFilter;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.mgnt.events.repositories.UserTokenRepository;
+import com.mgnt.events.services.JwtService;
+
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+  private final JwtService jwtService;
+  private final UserDetailsService userDetailsService;
+  private final UserTokenRepository tokenRepository;
+
+  public JwtAuthenticationFilter(
+    JwtService jwtService,
+    UserDetailsService userDetailsService,
+    UserTokenRepository tokenRepository
+  ) {
+    this.jwtService = jwtService;
+    this.userDetailsService = userDetailsService;
+    this.tokenRepository = tokenRepository;
+  }
+
   @Override
   protected void doFilterInternal(
     HttpServletRequest request,
