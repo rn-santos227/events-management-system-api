63b14f065511e88037b65018645e5e3aecc2eb46
rn-santos227 <rn_santos227@yahoo.com>
2025-10-31 21:47:05 +0800

    added constructor in security config file

--- src/main/java/com/mgnt/events/security/SecurityConfig.java
+++ src/main/java/com/mgnt/events/security/SecurityConfig.java
@@ -3,10 +3,22 @@ package com.mgnt.events.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.mgnt.events.security.auth.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-    
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final UserDetailsService userDetailsService;
+
+  public SecurityConfig(
+    JwtAuthenticationFilter jwtAuthenticationFilter,
+    UserDetailsService userDetailsService
+  ) {
+    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    this.userDetailsService = userDetailsService;
+  }
 }
