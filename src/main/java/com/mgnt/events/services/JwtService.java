4ae2bdafa6ae49b9ee9399033c511a83debfe716
rn-santos227 <rn_santos227@yahoo.com>
2025-10-31 20:55:58 +0800

    added constructor in jwt service file

--- src/main/java/com/mgnt/events/services/JwtService.java
+++ src/main/java/com/mgnt/events/services/JwtService.java
@@ -1,8 +1,18 @@
 package com.mgnt.events.services;
 
 import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
 
 public class JwtService {
   private final Algorithm algorithm;
   private final long expirationMillis;
+
+  public JwtService(
+    @Value("${app.security.jwt.secret}") String secret,
+    @Value("${app.security.jwt.expiration:3600000}") long expirationMillis
+  ) {
+    this.algorithm = Algorithm.HMAC256(secret);
+    this.expirationMillis = expirationMillis;
+  }
+
 }
