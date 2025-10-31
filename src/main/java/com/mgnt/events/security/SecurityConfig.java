ac016bb68112d354ad72bd052cabf344bb4fc0bf
rn-santos227 <rn_santos227@yahoo.com>
2025-11-01 00:41:13 +0800

    updated security filter chain function in security config file

--- src/main/java/com/mgnt/events/security/SecurityConfig.java
+++ src/main/java/com/mgnt/events/security/SecurityConfig.java
@@ -9,11 +9,14 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.mgnt.events.constants.Routes;
 import com.mgnt.events.security.auth.JwtAuthenticationFilter;
 
 @Configuration
@@ -33,7 +36,18 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+    return http
+      .csrf(csrf -> csrf.disable())
+      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .authorizeHttpRequests(auth ->
+        auth
+          .requestMatchers(Routes.AUTH + "/**").permitAll()
+          .anyRequest()
+          .authenticated()
+      )
+      .authenticationProvider(authenticationProvider())
+      .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+      .build();
   }
 
   @Bean
