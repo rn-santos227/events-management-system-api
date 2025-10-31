a2f4e8ffc4a21de28d6d73db6cb14fd923934eba
rn-santos227 <rn_santos227@yahoo.com>
2025-10-31 21:21:29 +0800

    added constructor in user details service impl file

--- src/main/java/com/mgnt/events/services/UserDetailsServiceImpl.java
+++ src/main/java/com/mgnt/events/services/UserDetailsServiceImpl.java
@@ -2,7 +2,13 @@ package com.mgnt.events.services;
 
 import org.springframework.stereotype.Service;
 
+import com.mgnt.events.repositories.UserRepository;
+
 @Service
 public class UserDetailsServiceImpl {
-    
+  private final UserRepository userRepository;
+  
+  public UserDetailsServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 }
