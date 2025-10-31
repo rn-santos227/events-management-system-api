f74a3ee01a9207dc3379a44f8698d9144760e685
rn-santos227 <rn_santos227@yahoo.com>
2025-11-01 00:32:56 +0800

    added authentication manager function in security config file

--- src/main/java/com/mgnt/events/security/SecurityConfig.java
+++ src/main/java/com/mgnt/events/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.mgnt.events.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,6 +25,12 @@ public class SecurityConfig {
   ) {
     this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     this.userDetailsService = userDetailsService;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+    throws Exception {
+    return config.getAuthenticationManager();
   }
 
   @Bean
