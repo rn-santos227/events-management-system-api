b9f331062f9fa9a6cd64fe165768f6134978e2f2
rn-santos227 <rn_santos227@yahoo.com>
2025-10-31 21:28:09 +0800

    added get authorize function in user model file

--- src/main/java/com/mgnt/events/models/User.java
+++ src/main/java/com/mgnt/events/models/User.java
@@ -8,9 +8,17 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.mgnt.events.constants.Attributes;
 import com.mgnt.events.constants.Defaults;
@@ -21,7 +29,7 @@ import com.mgnt.events.constants.Tables;
 @Table(name = Tables.USERS)
 @SQLDelete(sql = Queries.DELETE_TIMESTAMP)
 @SQLRestriction(Queries.DELETE_RESTRICTION)
-public class User extends AuditableEntity {
+public class User extends AuditableEntity implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -50,6 +58,13 @@ public class User extends AuditableEntity {
   )
   private String lastName;
 
+  @Column(
+    name = Attributes.CONTACT_NUMBER,
+    nullable = false,
+    length = Defaults.DEFAULT_PHONE_LENGTH
+  )
+  private String contactNumber;
+
   @ManyToOne
   @JoinColumn(name = Attributes.ROLE_ID, nullable = false)
   private Role role;
@@ -58,11 +73,19 @@ public class User extends AuditableEntity {
   private boolean active = true;
 
   public User() {}
-  public User(String email, String password, String firstName, String lastName, Role role) {
+  public User(
+    String email,
+    String password,
+    String firstName,
+    String lastName,
+    String contactNumber,
+    Role role
+  ) {
     this.email = email;
     this.password = password;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.contactNumber = contactNumber;
     this.role = role;
   }
 
@@ -73,15 +96,43 @@ public class User extends AuditableEntity {
     ).trim();
   }
 
+  @Override
+  public String getPassword() { return password; }
+  public void setPassword(String password) { this.password = password; }
+
+ @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    if (role == null) {
+      return Collections.emptySet();
+    }
+
+    Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+    if (role.getName() != null && !role.getName().isBlank()) {
+      authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName().toUpperCase()));
+    }
+
+    if (role.getPrivileges() != null && !role.getPrivileges().isEmpty()) {
+      authorities.addAll(
+        role
+          .getPrivileges()
+          .stream()
+          .filter(privilege -> privilege.getAction() != null)
+          .map(privilege -> new SimpleGrantedAuthority(privilege.getAction()))
+          .collect(Collectors.toSet())
+      );
+    }
+    return authorities;
+  }
+
   public Long getId() { return id; }
   public String getEmail() { return email; }
   public void setEmail(String email) { this.email = email; }
-  public String getPassword() { return password; }
-  public void setPassword(String password) { this.password = password; }
   public String getFirstName() { return firstName; }
   public void setFirstName(String firstName) { this.firstName = firstName; }
   public String getLastName() { return lastName; }
   public void setLastName(String lastName) { this.lastName = lastName; }
+  public String getContactNumber() { return contactNumber; }
+  public void setContactNumber(String contactNumber) { this.contactNumber = contactNumber; }
   public boolean isActive() { return active; }
   public void setActive(boolean active) { this.active = active; }
   public Role getRole() { return role; }
