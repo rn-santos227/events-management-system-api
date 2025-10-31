59fdd5d79cd1678a7c81470b70bded6a5bc119d7
rn-santos227 <rn_santos227@yahoo.com>
2025-10-31 21:23:12 +0800

    added load user by username function in user details service impl file

--- src/main/java/com/mgnt/events/services/UserDetailsServiceImpl.java
+++ src/main/java/com/mgnt/events/services/UserDetailsServiceImpl.java
@@ -1,14 +1,24 @@
 package com.mgnt.events.services;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.mgnt.events.repositories.UserRepository;
 
 @Service
-public class UserDetailsServiceImpl {
+public class UserDetailsServiceImpl implements UserDetailsService {
   private final UserRepository userRepository;
   
   public UserDetailsServiceImpl(UserRepository userRepository) {
     this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return userRepository
+      .findByEmail(username)
+      .orElseThrow(() -> new UsernameNotFoundException("User not found"));
   }
 }
