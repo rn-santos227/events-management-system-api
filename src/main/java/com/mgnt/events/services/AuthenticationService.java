9bc5abcc59e310efc8b42e0af4c6533963fba3c9
rn-santos227 <rn_santos227@yahoo.com>
2025-11-01 19:33:24 +0800

    added constructor in authentication service file

--- src/main/java/com/mgnt/events/services/AuthenticationService.java
+++ src/main/java/com/mgnt/events/services/AuthenticationService.java
@@ -10,4 +10,14 @@ public class AuthenticationService {
   private final AuthenticationManager authenticationManager;
   private final JwtService jwtService;
   private final UserTokenRepository tokenRepository;
+
+  public AuthenticationService(
+    AuthenticationManager authenticationManager,
+    JwtService jwtService,
+    UserTokenRepository tokenRepository
+  ) {
+    this.authenticationManager = authenticationManager;
+    this.jwtService = jwtService;
+    this.tokenRepository = tokenRepository;
+  }
 }
