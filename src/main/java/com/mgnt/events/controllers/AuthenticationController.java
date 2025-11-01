032c2780a33205471cd23a222ba260cba38442fe
rn-santos227 <rn_santos227@yahoo.com>
2025-11-01 19:29:29 +0800

    updated authentication controller file

--- src/main/java/com/mgnt/events/controllers/AuthenticationController.java
+++ src/main/java/com/mgnt/events/controllers/AuthenticationController.java
@@ -4,9 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mgnt.events.constants.Routes;
+import com.mgnt.events.services.AuthenticationService;
 
 @RestController
 @RequestMapping(Routes.AUTH)
 public class AuthenticationController {
-    
+  private final AuthenticationService authenticationService; 
 }
