7af3ec4eeeb8fca19acf9d8a614a5c6a02025cac
rn-santos227 <rn_santos227@yahoo.com>
2025-11-01 19:28:54 +0800

    created authentication controller file

--- src/main/java/com/mgnt/events/constants/Routes.java
+++ src/main/java/com/mgnt/events/constants/Routes.java
@@ -9,4 +9,6 @@ public class Routes {
   public static final String BASE_PATH = API_BASE + API_VERSION;
   public static final String TEST = BASE_PATH + "/test";
   public static final String AUTH = BASE_PATH + "/auth";
+  
+  public static final String LOGIN = "/login";
 }
