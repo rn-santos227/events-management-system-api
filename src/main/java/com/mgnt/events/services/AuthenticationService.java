389fc540fb0a03f883944d2f4ce2d32c08324ae1
rn-santos227 <rn_santos227@yahoo.com>
2025-11-01 19:34:49 +0800

    added authenticate function in authentication service file

--- src/main/java/com/mgnt/events/services/AuthenticationService.java
+++ src/main/java/com/mgnt/events/services/AuthenticationService.java
@@ -2,8 +2,11 @@ package com.mgnt.events.services;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mgnt.events.repositories.UserTokenRepository;
+import com.mgnt.events.requests.LoginRequest;
+import com.mgnt.events.security.auth.LoginResponse;
 
 @Service
 public class AuthenticationService {
@@ -19,5 +22,10 @@ public class AuthenticationService {
     this.authenticationManager = authenticationManager;
     this.jwtService = jwtService;
     this.tokenRepository = tokenRepository;
+  }
+
+  @Transactional
+  public LoginResponse authenticate(LoginRequest request) {
+    
   }
 }
