2a6fdb517473c5b1a67881ace13cabf963efd404
rn-santos227 <rn_santos227@yahoo.com>
2025-11-01 19:56:00 +0800

    updated user token model file

--- src/main/java/com/mgnt/events/models/UserToken.java
+++ src/main/java/com/mgnt/events/models/UserToken.java
@@ -11,8 +11,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 
 import com.mgnt.events.constants.Attributes;
 import com.mgnt.events.constants.Defaults;
@@ -21,43 +22,35 @@ import com.mgnt.events.types.TokenType;
 
 @Entity
 @Table(name = Tables.USER_TOKENS)
+@Getter
 public class UserToken {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Setter
   @Column(nullable = false, unique = false, length = Defaults.DEFAULT_TOKEN_LENGTH)
   private String token;
 
   @Enumerated(EnumType.STRING)
   @Column(name = Attributes.TOKEN_TYPE, nullable = false)
+  @Setter
   private TokenType tokenType = TokenType.BEARER;
 
+  @Setter
   @Column(nullable = false)
   private boolean revoked;
 
+  @Setter
   @Column(nullable = false)
   private boolean expired;
 
+  @Setter
   @Column(name = Attributes.EXPIRES_AT)
   private LocalDateTime expiresAt;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = Attributes.USER_ID, nullable = false)
+  @Setter
   private User user;
-
-
-  public Long getId() { return id; }
-  public String getToken() { return token; }
-  public void setToken(String token) { this.token = token; }
-  public TokenType getTokenType() { return tokenType; }
-  public void setTokenType(TokenType tokenType) { this.tokenType = tokenType; }
-  public boolean isRevoked() { return revoked; }
-  public void setRevoked(boolean revoked) { this.revoked = revoked; }
-  public boolean isExpired() { return expired; }
-  public void setExpired(boolean expired) { this.expired = expired; }
-  public LocalDateTime getExpiresAt() { return expiresAt; }
-  public void setExpiresAt(LocalDateTime expiresAt) { this.expiresAt = expiresAt; }
-  public User getUser() { return user; }
-  public void setUser(User user) { this.user = user; }
 }
