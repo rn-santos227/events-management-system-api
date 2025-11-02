0c67aa819d1ef72ba85c95a709884755ad0a109c
rn-santos227 <rn_santos227@yahoo.com>
2025-11-02 17:05:31 +0800

    updated queries constant file

--- src/main/java/com/mgnt/events/constants/Queries.java
+++ src/main/java/com/mgnt/events/constants/Queries.java
@@ -3,5 +3,10 @@ package com.mgnt.events.constants;
 public class Queries {
   Queries() {}
   public static final String DELETE_RESTRICTION = "deleted_at IS NULL";
-  public static final String DELETE_TIMESTAMP = "UPDATE privileges SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?";
+  public static final String DELETE_PRIVILEGES =
+    "UPDATE privileges SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?";
+  public static final String DELETE_ROLES =
+    "UPDATE roles SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?";
+  public static final String DELETE_USERS =
+    "UPDATE users SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?";
 }
