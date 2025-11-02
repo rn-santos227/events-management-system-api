e6a6cd4d79f415dd50394ecd212d9ed597a046b8
rn-santos227 <rn_santos227@yahoo.com>
2025-11-02 17:13:48 +0800

    created privilege service file

--- src/main/java/com/mgnt/events/constants/Attributes.java
+++ src/main/java/com/mgnt/events/constants/Attributes.java
@@ -9,6 +9,7 @@ public class Attributes {
   public static final String CONTACT_NUMBER = "contact_number";
   public static final String FIRST_NAME = "first_name";
   public static final String LAST_NAME = "last_name";
+  public static final String NAME = "name";
   public static final String ROLE = "role";
 
   public static final String TOKEN_TYPE = "token_type";
