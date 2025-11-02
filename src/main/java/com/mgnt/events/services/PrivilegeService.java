ecad5e46db772cf3eeed37522698d2400e0c6cf7
rn-santos227 <rn_santos227@yahoo.com>
2025-11-02 17:40:19 +0800

    added constructor in privilge service file

--- src/main/java/com/mgnt/events/services/PrivilegeService.java
+++ src/main/java/com/mgnt/events/services/PrivilegeService.java
@@ -4,8 +4,14 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.mgnt.events.constants.Attributes;
+import com.mgnt.events.repositories.PrivilegeRepository;
 
 @Service
 public class PrivilegeService {
   private static final Sort DEFA_SORT = Sort.by(Sort.Direction.ASC, Attributes.NAME);
+  private final PrivilegeRepository privilegeRepository;
+
+  public PrivilegeService(PrivilegeRepository privilegeRepository) {
+    this.privilegeRepository = privilegeRepository;
+  }  
 }
