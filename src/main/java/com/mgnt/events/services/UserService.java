4f8aba7a76ca09252da63200adf1cab4ed90e4f0
rn-santos227 <rn_santos227@yahoo.com>
2025-11-03 19:11:46 +0800

    added constructor in user service file

--- src/main/java/com/mgnt/events/services/UserService.java
+++ src/main/java/com/mgnt/events/services/UserService.java
@@ -1,11 +1,28 @@
 package com.mgnt.events.services;
 
 import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.mgnt.events.constants.Attributes;
+import com.mgnt.events.repositories.RoleRepository;
+import com.mgnt.events.repositories.UserRepository;
 
 @Service
 public class UserService {
   private static final Sort DEFAULT_SORT = Sort.by(Sort.Direction.ASC, Attributes.EMAIL);
+
+  private final UserRepository userRepository;
+  private final RoleRepository roleRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  public UserService(
+    UserRepository userRepository,
+    RoleRepository roleRepository,
+    PasswordEncoder passwordEncoder
+  ) {
+    this.userRepository = userRepository;
+    this.roleRepository = roleRepository;
+    this.passwordEncoder = passwordEncoder;
+  }
 }
