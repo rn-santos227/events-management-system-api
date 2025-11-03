82d4981b51bd6237bc319137416ba0d4c993ba2e
rn-santos227 <rn_santos227@yahoo.com>
2025-11-03 19:17:16 +0800

    added find all and find by id functions in user srvice file

--- src/main/java/com/mgnt/events/services/UserService.java
+++ src/main/java/com/mgnt/events/services/UserService.java
@@ -1,9 +1,13 @@
 package com.mgnt.events.services;
 
+import java.util.List;
+
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.mgnt.events.constants.Attributes;
@@ -16,6 +20,7 @@ import com.mgnt.events.responses.users.UserResponse;
 
 @Service
 public class UserService {
+  @NonNull
   private static final Sort DEFAULT_SORT = Sort.by(Sort.Direction.ASC, Attributes.EMAIL);
 
   private final UserRepository userRepository;
@@ -30,6 +35,16 @@ public class UserService {
     this.userRepository = userRepository;
     this.roleRepository = roleRepository;
     this.passwordEncoder = passwordEncoder;
+  }
+
+  @Transactional(readOnly = true)
+  public List<UserResponse> findAll() {
+    return userRepository.findAll(DEFAULT_SORT).stream().map(this::toResponse).toList();
+  }
+
+  @Transactional(readOnly = true)
+  public UserResponse findById(Long id) {
+    return toResponse(getUser(id));
   }
 
   private User getUser(Long id) {
