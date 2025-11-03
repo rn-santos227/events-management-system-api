e38bee1a90de47d095a2c794dfbb83f17b6bb719
rn-santos227 <rn_santos227@yahoo.com>
2025-11-03 18:30:26 +0800

    added constructor in role service file

--- src/main/java/com/mgnt/events/services/RoleService.java
+++ src/main/java/com/mgnt/events/services/RoleService.java
@@ -4,8 +4,17 @@ import org.springframework.data.domain.Sort;
 import org.springframework.lang.NonNull;
 
 import com.mgnt.events.constants.Attributes;
+import com.mgnt.events.repositories.PrivilegeRepository;
+import com.mgnt.events.repositories.RoleRepository;
 
 public class RoleService {
   @NonNull
   private static final Sort DEFAULT_SORT = Sort.by(Sort.Direction.ASC, Attributes.NAME);
+  private final RoleRepository roleRepository;
+  private final PrivilegeRepository privilegeRepository;
+
+  public RoleService(RoleRepository roleRepository, PrivilegeRepository privilegeRepository) {
+    this.roleRepository = roleRepository;
+    this.privilegeRepository = privilegeRepository;
+  }
 }
