820689e1582c86b64c9c7701da8d340c675f8450
rn-santos227 <rn_santos227@yahoo.com>
2025-11-04 01:48:32 +0800

    override require non null function in  validations utility file

--- src/main/java/com/mgnt/events/utils/RequestValidators.java
+++ src/main/java/com/mgnt/events/utils/RequestValidators.java
@@ -1,11 +1,16 @@
 package com.mgnt.events.utils;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.lang.Nullable;
 import org.springframework.web.server.ResponseStatusException;
 
 public final class RequestValidators {
   private RequestValidators() {}
+
+  public static <T> T requireNonNull(@Nullable T value, String attributeName) {
+    return requireNonNull(value, attributeName, HttpStatus.BAD_REQUEST);
+  }
 
   public static <T> T requireNonNull(
     @Nullable T value,
