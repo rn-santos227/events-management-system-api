074c21ea7028ddc5c677fba2a4c710c91bb6c4d0
rn-santos227 <rn_santos227@yahoo.com>
2025-11-03 19:14:45 +0800

    added get role function in user service file

--- src/main/java/com/mgnt/events/services/UserService.java
+++ src/main/java/com/mgnt/events/services/UserService.java
@@ -1,8 +1,10 @@
 package com.mgnt.events.services;
 
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.mgnt.events.constants.Attributes;
 import com.mgnt.events.models.Role;
@@ -28,6 +30,12 @@ public class UserService {
     this.userRepository = userRepository;
     this.roleRepository = roleRepository;
     this.passwordEncoder = passwordEncoder;
+  }
+
+  private Role getRole(Long id) {
+    return roleRepository
+      .findById(id)
+      .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Role not found"));
   }
 
   private UserResponse toResponse(User user) {
