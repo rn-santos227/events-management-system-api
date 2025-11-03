f12b48da525912bf7b9f118141d5e2bdc4405f08
rn-santos227 <rn_santos227@yahoo.com>
2025-11-04 01:48:11 +0800

    added require non null function in  validations utility file

--- src/main/java/com/mgnt/events/utils/RequestValidators.java
+++ src/main/java/com/mgnt/events/utils/RequestValidators.java
@@ -1,9 +1,26 @@
 package com.mgnt.events.utils;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.lang.Nullable;
 import org.springframework.web.server.ResponseStatusException;
 
 public final class RequestValidators {
   private RequestValidators() {}
+
+  public static <T> T requireNonNull(
+    @Nullable T value,
+    String attributeName,
+    HttpStatusCode status
+  ) {
+    if (status == null) {
+      throw new IllegalArgumentException("HttpStatusCode must not be null");
+    }
+    if (value == null) {
+      throw new ResponseStatusException(
+        status,
+        String.format("%s must not be null", attributeName)
+      );
+    }
+    return value;
+  }
 }
