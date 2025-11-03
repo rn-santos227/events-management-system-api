580886b5470ef0a442637c5882540771b8ccb10b
rn-santos227 <rn_santos227@yahoo.com>
2025-11-03 19:19:48 +0800

    created user create request file

---
+++ src/main/java/com/mgnt/events/requests/users/UserCreateRequest.java
@@ -0,0 +1,15 @@
+package com.mgnt.events.requests.users;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record UserCreateRequest(
+  @NotBlank(message = "Email is required") @Email(message = "Email must be valid") String email,
+  @NotBlank(message = "Password is required") String password,
+  @NotBlank(message = "First name is required") String firstName,
+  @NotBlank(message = "Last name is required") String lastName,
+  @NotBlank(message = "Contact number is required") String contactNumber,
+  @NotNull(message = "Role is required") Long roleId,
+  Boolean active
+) {}
