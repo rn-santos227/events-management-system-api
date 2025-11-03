da7ccca46b6d62d341738382c91a8517c6fd6c1b
rn-santos227 <rn_santos227@yahoo.com>
2025-11-04 01:40:41 +0800

    created request validations utility file

---
+++ src/main/java/com/mgnt/events/utils/RequestValidators.java
@@ -0,0 +1,9 @@
+package com.mgnt.events.utils;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
+import org.springframework.web.server.ResponseStatusException;
+
+public final class RequestValidators {
+  private RequestValidators() {}
+}
