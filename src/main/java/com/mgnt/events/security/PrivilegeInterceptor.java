472300f153c4ff7b90b83ded90276b4417e4d398
rn-santos227 <rn_santos227@yahoo.com>
2025-11-05 00:24:58 +0800

    added override pre handle function in privilege interceptor file

--- src/main/java/com/mgnt/events/security/PrivilegeInterceptor.java
+++ src/main/java/com/mgnt/events/security/PrivilegeInterceptor.java
@@ -1,9 +1,20 @@
 package com.mgnt.events.security;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @Component
 public class PrivilegeInterceptor extends HandlerInterceptor {
+  @Override
+  public boolean preHandle(
+    @NonNull HttpServletRequest request,
+    @NonNull HttpServletResponse response,
+    @NonNull Object handler
+  ) throws Exception {
     
+  }
 }
