5daab2f5e089f6d945874eb0aa1b44765e81c6c1
rn-santos227 <rn_santos227@yahoo.com>
2025-11-05 00:19:08 +0800

    created privilige interceptor file in security

---
+++ src/main/java/com/mgnt/events/security/PrivilegeInterceptor.java
@@ -0,0 +1,9 @@
+package com.mgnt.events.security;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class PrivilegeInterceptor extends HandlerInterceptor {
+    
+}
