e2282590712f91b49bb87ffdeaff3ca3f4bbdf82
rn-santos227 <rn_santos227@yahoo.com>
2025-11-05 00:16:47 +0800

    updated requires privilege file in security

--- src/main/java/com/mgnt/events/security/annotations/RequiresPrivilege.java
+++ src/main/java/com/mgnt/events/security/annotations/RequiresPrivilege.java
@@ -1,9 +1,12 @@
 package com.mgnt.events.security.annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target({ ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RequiresPrivilege {
     
 }
