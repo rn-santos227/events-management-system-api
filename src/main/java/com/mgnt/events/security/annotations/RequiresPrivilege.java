22c998ce3bed1d9b9b3b144757f41e3f635fcdda
rn-santos227 <rn_santos227@yahoo.com>
2025-11-05 00:16:22 +0800

    created requires privilege file in security

---
+++ src/main/java/com/mgnt/events/security/annotations/RequiresPrivilege.java
@@ -0,0 +1,9 @@
+package com.mgnt.events.security.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.METHOD, ElementType.TYPE})
+public @interface RequiresPrivilege {
+    
+}
