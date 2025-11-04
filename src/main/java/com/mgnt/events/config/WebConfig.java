ec8d9f6af3a03a73d18609a00563ca47ab0abafa
rn-santos227 <rn_santos227@yahoo.com>
2025-11-05 00:59:50 +0800

    updated web config file

--- src/main/java/com/mgnt/events/config/WebConfig.java
+++ src/main/java/com/mgnt/events/config/WebConfig.java
@@ -1,15 +1,22 @@
 package com.mgnt.events.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.mgnt.events.security.PrivilegeInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-  private final PrivilegeInterceptor privilegeInterceptor;
+  private final @NonNull PrivilegeInterceptor privilegeInterceptor;
 
-  public WebConfig(PrivilegeInterceptor privilegeInterceptor) {
+  public WebConfig(@NonNull PrivilegeInterceptor privilegeInterceptor) {
     this.privilegeInterceptor = privilegeInterceptor;
+  }
+
+  @Override
+  public void addInterceptors(@NonNull InterceptorRegistry registry) {
+    registry.addInterceptor(privilegeInterceptor);
   }
 }
