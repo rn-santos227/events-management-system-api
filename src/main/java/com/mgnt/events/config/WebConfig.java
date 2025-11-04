c974e72f8fec0ff734b039ee9b1bab3eb129fa6b
rn-santos227 <rn_santos227@yahoo.com>
2025-11-05 00:40:07 +0800

    added constructor in web config file

--- src/main/java/com/mgnt/events/config/WebConfig.java
+++ src/main/java/com/mgnt/events/config/WebConfig.java
@@ -2,7 +2,13 @@ package com.mgnt.events.config;
 
 import org.springframework.context.annotation.Configuration;
 
+import com.mgnt.events.security.PrivilegeInterceptor;
+
 @Configuration
 public class WebConfig {
-    
+  private final PrivilegeInterceptor privilegeInterceptor;
+
+  public WebConfig(PrivilegeInterceptor privilegeInterceptor) {
+    this.privilegeInterceptor = privilegeInterceptor;
+  }
 }
