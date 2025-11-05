282ab79d2649a8627299af77981618d2eaf02887
rn-santos227 <rn_santos227@yahoo.com>
2025-11-05 20:39:13 +0800

    added constructor in controller file

--- src/main/java/com/mgnt/events/controllers/RoleController.java
+++ src/main/java/com/mgnt/events/controllers/RoleController.java
@@ -4,9 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mgnt.events.constants.Routes;
+import com.mgnt.events.services.RoleService;
 
 @RestController
 @RequestMapping(Routes.ROLES)
 public class RoleController {
-    
+  private final RoleService _roleService;
+
+  public RoleController(RoleService roleService) {
+    this._roleService = roleService;
+  }
 }
