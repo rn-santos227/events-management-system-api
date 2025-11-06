e3a8fe142d0c9ad2e1ef651f1f60a47d7e5d041a
rn-santos227 <rn_santos227@yahoo.com>
2025-11-06 21:14:20 +0800

    added constructor in user controller file

--- src/main/java/com/mgnt/events/controllers/UserController.java
+++ src/main/java/com/mgnt/events/controllers/UserController.java
@@ -4,9 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mgnt.events.constants.Routes;
+import com.mgnt.events.services.UserService;
 
 @RestController
 @RequestMapping(Routes.USERS)
 public class UserController {
-    
+  private final UserService _userService;
+
+  public UserController(UserService userService) {
+    this._userService = userService;
+  }
 }
