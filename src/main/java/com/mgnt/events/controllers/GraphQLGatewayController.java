ef6351914ec665c5a763befbe856e004576ac8cd
rn-santos227 <rn_santos227@yahoo.com>
2025-11-07 00:54:52 +0800

    updated graphql gateway controller file

--- src/main/java/com/mgnt/events/controllers/GraphQLGatewayController.java
+++ src/main/java/com/mgnt/events/controllers/GraphQLGatewayController.java
@@ -5,12 +5,14 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import com.mgnt.events.constants.Queries;
 import com.mgnt.events.responses.privileges.PrivilegeResponse;
 import com.mgnt.events.responses.roles.RoleResponse;
 import com.mgnt.events.responses.users.UserResponse;
 import com.mgnt.events.services.PrivilegeService;
 import com.mgnt.events.services.RoleService;
 import com.mgnt.events.services.UserService;
+import com.mgnt.events.utils.RequestValidators;
 
 @Controller
 public class GraphQLGatewayController {
@@ -35,7 +37,8 @@ public class GraphQLGatewayController {
 
   @QueryMapping
   public List<UserResponse> users(@Argument Integer limit) {
-    return _userService.findAll(limit);
+    Integer sanitizedLimit = sanitizeLimit(limit);
+    return _userService.findAll(sanitizedLimit);
   }
 
   @QueryMapping
@@ -45,7 +48,8 @@ public class GraphQLGatewayController {
 
   @QueryMapping
   public List<RoleResponse> roles(@Argument Integer limit) {
-    return _roleService.findAll(limit);
+    Integer sanitizedLimit = sanitizeLimit(limit);
+    return _roleService.findAll(sanitizedLimit);
   }
 
   @QueryMapping
@@ -55,11 +59,16 @@ public class GraphQLGatewayController {
 
   @QueryMapping
   public List<PrivilegeResponse> privileges(@Argument Integer limit) {
-    return _privilegeService.findAll(limit);
+    Integer sanitizedLimit = sanitizeLimit(limit);
+    return _privilegeService.findAll(sanitizedLimit);
   }
 
   @QueryMapping
   public PrivilegeResponse privilege(@Argument long id) {
     return _privilegeService.findById(id);
+  }
+
+  private Integer sanitizeLimit(Integer limit) {
+    return RequestValidators.requirePositiveOrNull(limit, Queries.LIMIT);
   }
 }
