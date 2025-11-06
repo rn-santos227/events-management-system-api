875c2b629ce2644adc29337b59f66a5db27f17ea
rn-santos227 <rn_santos227@yahoo.com>
2025-11-07 00:42:56 +0800

    added require positive or null function in request validators util file

--- src/main/java/com/mgnt/events/utils/RequestValidators.java
+++ src/main/java/com/mgnt/events/utils/RequestValidators.java
@@ -1,5 +1,9 @@
 package com.mgnt.events.utils;
 
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
@@ -12,7 +16,7 @@ public final class RequestValidators {
   public static <T> T requireNonNull(@Nullable T value, String attributeName) {
     return requireNonNull(value, attributeName, HttpStatus.BAD_REQUEST);
   }
-  
+
   @NonNull
   public static <T> T requireNonNull(
     @Nullable T value,
@@ -33,6 +37,14 @@ public final class RequestValidators {
 
   public static int requirePositive(Integer value, String attributeName) {
     return requirePositive(value, attributeName, HttpStatus.BAD_REQUEST);
+  }
+
+  @Nullable
+  public static Integer requirePositiveOrNull(@Nullable Integer value, String attributeName) {
+    if (value == null) {
+      return null;
+    }
+    return requirePositive(value, attributeName);
   }
 
   public static int requirePositive(Integer value, String attributeName, HttpStatus status) {
