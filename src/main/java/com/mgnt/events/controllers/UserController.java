c44e0d398e8172773fd87761e980100d67460b7f
rn-santos227 <rn_santos227@yahoo.com>
2025-11-06 21:13:11 +0800

    created user controller file

---
+++ src/main/java/com/mgnt/events/controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.mgnt.events.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mgnt.events.constants.Routes;
+
+@RestController
+@RequestMapping(Routes.USERS)
+public class UserController {
+    
+}
