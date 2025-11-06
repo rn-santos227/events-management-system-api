9c2bf17b5255be02e7c60ac5790ee2935f9a84fd
rn-santos227 <rn_santos227@yahoo.com>
2025-11-06 21:02:53 +0800

    added find by id function in role controller file

--- src/main/java/com/mgnt/events/controllers/RoleController.java
+++ src/main/java/com/mgnt/events/controllers/RoleController.java
@@ -2,7 +2,9 @@ package com.mgnt.events.controllers;
 
 import java.util.List;
 
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,10 @@ public class RoleController {
   @GetMapping
   public List<RoleResponse> findAll() {
     return _roleService.findAll();
+  }
+
+  @GetMapping(Routes.APPEND_ID)
+  public RoleResponse findById(@PathVariable @NonNull Long id) {
+    return _roleService.findById(id);
   }
 }
