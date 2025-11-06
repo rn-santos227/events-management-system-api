317252bdb79707e65382765a7b441ee36fbe68dd
rn-santos227 <rn_santos227@yahoo.com>
2025-11-07 00:05:26 +0800

    updated storage properties file

--- src/main/java/com/mgnt/events/config/storage/StorageProperties.java
+++ src/main/java/com/mgnt/events/config/storage/StorageProperties.java
@@ -8,5 +8,10 @@ import com.mgnt.events.constants.Storage;
 @Validated
 @ConfigurationProperties(prefix = Storage.PROPERTY_PREFIX)
 public class StorageProperties {
-    
+  private StorageProvider _provider = StorageProvider.S3;
+  private String _accessKey;
+  private String _bucket;
+  private String _secretKey;
+  private String _endpoint;
+  private boolean _enabled;
 }
