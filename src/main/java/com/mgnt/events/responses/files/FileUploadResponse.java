288e6d428bb2d38e7da6c77281c7bfbd8903b696
rn-santos227 <rn_santos227@yahoo.com>
2025-11-07 21:27:23 +0800

    created files upload response file

---
+++ src/main/java/com/mgnt/events/responses/files/FileUploadResponse.java
@@ -0,0 +1,17 @@
+package com.mgnt.events.responses.files;
+
+import java.time.LocalDateTime;
+
+public record FileUploadResponse(
+  Long id,
+  String fileName,
+  String fileUrl,
+  String bucket,
+  Long size,
+  String contentType,
+  String notes,
+  String url,
+  LocalDateTime createdAt,
+  LocalDateTime updatedAt,
+  LocalDateTime deletedAt
+) {}
