1118658cd496f33ca8f06fe1f08cd6f926ac993d
rn-santos227 <rn_santos227@yahoo.com>
2025-11-08 19:15:33 +0800

    added constructor in file storage service file

--- src/main/java/com/mgnt/events/services/FileStorageService.java
+++ src/main/java/com/mgnt/events/services/FileStorageService.java
@@ -1,11 +1,31 @@
 package com.mgnt.events.services;
 
 import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
+import com.mgnt.events.config.storage.StorageProperties;
 import com.mgnt.events.constants.Formats;
+import com.mgnt.events.repositories.StoredFileRepository;
+
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Service
 public class FileStorageService {
   private static final DateTimeFormatter _DIRECTORY_FORMATTER = DateTimeFormatter.ofPattern(Formats.DATE_FORMAT);
+
+  private final ObjectProvider<S3Client> _s3ClientProvider;
+  private final StorageProperties _storageProperties;
+  private final StoredFileRepository _storedFileRepository;
+
+  public FileStorageService(
+    ObjectProvider<S3Client> s3ObjectProvider,
+    StorageProperties storageProperties,
+    StoredFileRepository storedFileRepository
+  ) {
+    this._s3ClientProvider = s3ObjectProvider;
+    this._storageProperties = storageProperties;
+    this._storedFileRepository = storedFileRepository;
+  }
 }
