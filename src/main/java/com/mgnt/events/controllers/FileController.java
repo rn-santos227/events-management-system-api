fa7052c1536668977c716ce63966b96051ba5ae7
rn-santos227 <rn_santos227@yahoo.com>
2025-11-08 22:46:47 +0800

    added find all function in file controller file

--- src/main/java/com/mgnt/events/controllers/FileController.java
+++ src/main/java/com/mgnt/events/controllers/FileController.java
@@ -1,10 +1,17 @@
 package com.mgnt.events.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mgnt.events.constants.Queries;
 import com.mgnt.events.constants.Routes;
+import com.mgnt.events.responses.files.FileUploadResponse;
 import com.mgnt.events.services.FileStorageService;
+import com.mgnt.events.util.RequestValidators;
 
 @RestController
 @RequestMapping(Routes.FILES)
@@ -12,5 +19,11 @@ public class FileController {
   private final FileStorageService _fileStorageService;
   public FileController(FileStorageService fileStorageService) {
     this._fileStorageService = fileStorageService;
+  }
+
+  @GetMapping 
+  List<FileUploadResponse> findAll(@RequestParam(name = Queries.LIMIT, required = false) Integer limit) {
+    Integer sanitizedLimit = RequestValidators.requirePositiveOrNull(limit, Queries.LIMIT);
+    return _fileStorageService.findAll(sanitizedLimit);
   }
 }
