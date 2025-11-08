3d5bb2369e3a53deff296dafc4d54059019585cd
rn-santos227 <rn_santos227@yahoo.com>
2025-11-08 22:28:29 +0800

    added constructor in file controller file

--- src/main/java/com/mgnt/events/controllers/FileController.java
+++ src/main/java/com/mgnt/events/controllers/FileController.java
@@ -4,9 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mgnt.events.constants.Routes;
+import com.mgnt.events.services.FileStorageService;
 
 @RestController
 @RequestMapping(Routes.FILES)
 public class FileController {
-    
+  private final FileStorageService _fileStorageService;
+  public FileController(FileStorageService fileStorageService) {
+    this._fileStorageService = fileStorageService;
+  }
 }
