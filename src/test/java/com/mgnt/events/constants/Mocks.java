3f04ca061a8e57951dc15c004bfd1f3ba2be7e3c
rn-santos227 <rn_santos227@yahoo.com>
2025-11-09 15:48:46 +0800

    added auth class in mocks constant file

--- src/test/java/com/mgnt/events/constants/Mocks.java
+++ src/test/java/com/mgnt/events/constants/Mocks.java
@@ -2,4 +2,13 @@ package com.mgnt.events.constants;
 
 public final class Mocks {
   private Mocks() {}
+
+  public static final class Auth {
+    public static final String EMAIL = "user@example.com";
+    public static final String PASSWORD = "password123";
+    public static final String TOKEN_VALUE = "token-value";
+    public static final String TOKEN_TYPE = "Bearer";
+
+    private Auth() {}
+  }
 }
