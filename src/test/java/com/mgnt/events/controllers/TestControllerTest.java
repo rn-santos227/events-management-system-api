9c29caa7a26bb5dde8fa0c34ff7e292d04803660
rn-santos227 <rn_santos227@yahoo.com>
2025-11-09 17:46:41 +0800

    created text controller test file

---
+++ src/test/java/com/mgnt/events/controllers/TestControllerTest.java
@@ -0,0 +1,17 @@
+package com.mgnt.events.controllers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ExtendWith(MockitoExtension.class)
+public class TestControllerTest {
+  private MockMvc _mockMvc;
+  private ObjectMapper _objectMapper;
+
+  @InjectMocks
+  private TestController _testController;
+}
