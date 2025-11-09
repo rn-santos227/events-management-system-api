8d08082f2e11c6a4802eb61708d7ae7c0cfaa2e4
rn-santos227 <rn_santos227@yahoo.com>
2025-11-09 15:54:26 +0800

    updated json paths constant file

--- src/test/java/com/mgnt/events/constants/JsonPaths.java
+++ src/test/java/com/mgnt/events/constants/JsonPaths.java
@@ -2,4 +2,8 @@ package com.mgnt.events.constants;
 
 public final class JsonPaths {
   private JsonPaths() {}
+
+  public static final String ACCESS_TOKEN = "$.accessToken";
+  public static final String TOKEN_TYPE = "$.tokenType";
+  public static final String EXPIRES_AT = "$.expiresAt";
 }
