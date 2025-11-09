2abd8b8756b6138e730e7fb92c73283a97acabe7
rn-santos227 <rn_santos227@yahoo.com>
2025-11-09 16:01:10 +0800

    updated login should return access token test function in authentication controller test file

--- src/test/java/com/mgnt/events/controllers/AuthenticationControllerTest.java
+++ src/test/java/com/mgnt/events/controllers/AuthenticationControllerTest.java
@@ -1,21 +1,29 @@
 package com.mgnt.events.controllers;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mgnt.events.constants.JsonPaths;
 import com.mgnt.events.constants.Mocks;
+import com.mgnt.events.constants.Routes;
 import com.mgnt.events.requests.auth.LoginRequest;
 import com.mgnt.events.responses.auth.LoginResponse;
 import com.mgnt.events.services.AuthenticationService;
@@ -53,5 +61,19 @@ public class AuthenticationControllerTest {
     LoginRequest request = new LoginRequest();
     request.setEmail(Mocks.Auth.EMAIL);
     request.setPassword(Mocks.Auth.PASSWORD);
+
+    _mockMvc.perform(
+      post(Routes.AUTH_LOGIN)
+        .contentType(
+          RequestValidators.requireNonNull(MediaType.APPLICATION_JSON, "Media Type")
+        )
+        .content(
+          RequestValidators.requireNonNull(_objectMapper.writeValueAsString(request), "Request")
+        )
+    )
+    .andExpect(status().isOk())
+    .andExpect(jsonPath(JsonPaths.ACCESS_TOKEN).value(Mocks.Auth.TOKEN_VALUE))
+    .andExpect(jsonPath(JsonPaths.TOKEN_TYPE).value(Mocks.Auth.TOKEN_TYPE))
+    .andExpect(jsonPath(JsonPaths.EXPIRES_AT).value(1_720_000_000L));
   }
 }
