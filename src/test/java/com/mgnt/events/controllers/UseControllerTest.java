edfc0f9842739013361366852bd7f192589d2d40
rn-santos227 <rn_santos227@yahoo.com>
2025-11-10 20:23:15 +0800

    updated setup function in user controller test file

--- src/test/java/com/mgnt/events/controllers/UseControllerTest.java
+++ src/test/java/com/mgnt/events/controllers/UseControllerTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.mgnt.events.services.UserService;
+import com.mgnt.events.util.RequestValidators;
 
 @ExtendWith(MockitoExtension.class)
 public class UseControllerTest {
@@ -26,5 +29,12 @@ public class UseControllerTest {
   void setUp() {
     _objectMapper = new ObjectMapper();
     _objectMapper.registerModule(new JavaTimeModule());
+
+    _mockMvc = MockMvcBuilders
+      .standaloneSetup(_userController)
+      .setMessageConverters(new MappingJackson2HttpMessageConverter(
+        RequestValidators.requireNonNull(_objectMapper, "Object Mapper")
+      ))
+      .build();
   }
 }
