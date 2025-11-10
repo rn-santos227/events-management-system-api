9d4a69d0106eaeed03be4bb54463a6ff6db032d0
rn-santos227 <rn_santos227@yahoo.com>
2025-11-10 19:21:09 +0800

    updated create should forward request to service test function in role controller test file

--- src/test/java/com/mgnt/events/controllers/RoleControllerTest.java
+++ src/test/java/com/mgnt/events/controllers/RoleControllerTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,6 +21,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -87,6 +89,17 @@ public class RoleControllerTest {
     RoleRequest request = new RoleRequest(Mocks.Roles.NAME_ADMIN, Set.of(1L, 2L));
     RoleResponse response = new RoleResponse(1L, Mocks.Roles.NAME_ADMIN, Set.of(), LocalDateTime.now(), LocalDateTime.now());
     when(_roleService.create(any(RoleRequest.class))).thenReturn(response);
+
+    _mockMvc
+      .perform(
+        post(Routes.ROLES)
+        .contentType(
+          RequestValidators.requireNonNull(MediaType.APPLICATION_JSON, "Media Type")
+        )
+        .content(
+          RequestValidators.requireNonNull(_objectMapper.writeValueAsString(request), "Request")
+        )
+      );
   }
 
   @Test
