09a493a5ec234ba7b71a4f7fdafcf92b3ec01d97
rn-santos227 <rn_santos227@yahoo.com>
2025-11-10 18:37:36 +0800

    updated setup function in role controller test file

--- src/test/java/com/mgnt/events/controllers/RoleControllerTest.java
+++ src/test/java/com/mgnt/events/controllers/RoleControllerTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.mgnt.events.services.RoleService;
+import com.mgnt.events.util.RequestValidators;
 
 @ExtendWith(MockitoExtension.class)
 public class RoleControllerTest {
@@ -23,8 +26,15 @@ public class RoleControllerTest {
   private RoleController _roleController;
 
   @BeforeEach
-  void setUp() {
+  void setup() {
     _objectMapper = new ObjectMapper();
     _objectMapper.registerModule(new JavaTimeModule());
+
+    _mockMvc = MockMvcBuilders
+    .standaloneSetup(_roleController)
+    .setMessageConverters(new MappingJackson2HttpMessageConverter(
+      RequestValidators.requireNonNull(_objectMapper, "Object Mapper")
+    ))
+    .build();
   }
 }
