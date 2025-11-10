da8ebe2bbc29705719db7920fbb98393a38c6f18
rn-santos227 <rn_santos227@yahoo.com>
2025-11-10 18:53:06 +0800

    updated json paths constant file

--- src/test/java/com/mgnt/events/constants/JsonPaths.java
+++ src/test/java/com/mgnt/events/constants/JsonPaths.java
@@ -7,4 +7,11 @@ public final class JsonPaths {
   public static final String TOKEN_TYPE = "$.tokenType";
   public static final String EXPIRES_AT = "$.expiresAt";
   public static final String MESSAGE = "$.message";
+  public static final String ID = "$.id";
+  public static final String NAME = "$.name";
+
+  public static final String PAGINATION = "5";
+  public static final String INDEX_0_ID = "$[0].id";
+  public static final String INDEX_0_NAME = "$[0].name";
+  public static final String INDEX_0_PRIVILEGES = "$[0].privileges[*].name";
 }
