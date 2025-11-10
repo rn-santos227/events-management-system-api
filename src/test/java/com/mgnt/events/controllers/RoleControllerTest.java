df396c371206bf497970ffe7c5813212e5963223
rn-santos227 <rn_santos227@yahoo.com>
2025-11-10 18:55:01 +0800

    updated find all should return role responses test function in role controller test file

--- src/test/java/com/mgnt/events/controllers/RoleControllerTest.java
+++ src/test/java/com/mgnt/events/controllers/RoleControllerTest.java
@@ -1,5 +1,9 @@
 package com.mgnt.events.controllers;
 
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +16,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mgnt.events.constants.Mocks;
+import com.mgnt.events.responses.privileges.PrivilegeSummary;
 import com.mgnt.events.services.RoleService;
 import com.mgnt.events.util.RequestValidators;
 
@@ -41,6 +47,12 @@ public class RoleControllerTest {
 
   @Test
   void findAll_shoudReturnRoleResponses() throws Exception {
-
+    Set<PrivilegeSummary> _privileges = new LinkedHashSet<>(
+      List.of(new PrivilegeSummary(1L, 
+        Mocks.Roles.PRIVILEGE_MANAGE_USERS, 
+        Mocks.Roles.PRIVILEGE_ACTION_WRITE, 
+        Mocks.Roles.PRIVILEGE_SCOPE_USERS
+      ))
+    );
   }
 }
