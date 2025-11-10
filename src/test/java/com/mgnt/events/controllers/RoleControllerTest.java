8c349828da99bcfaf46003e322b39e2aa4de3c05
rn-santos227 <rn_santos227@yahoo.com>
2025-11-10 18:32:41 +0800

    updated role controller test file

--- src/test/java/com/mgnt/events/controllers/RoleControllerTest.java
+++ src/test/java/com/mgnt/events/controllers/RoleControllerTest.java
@@ -1,13 +1,22 @@
 package com.mgnt.events.controllers;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mgnt.events.services.RoleService;
 
 @ExtendWith(MockitoExtension.class)
 public class RoleControllerTest {
   private MockMvc _mockMvc;
   private ObjectMapper _objectMapper;
+
+  @Mock
+  private RoleService _roleService;
+
+  @InjectMocks
+  private RoleController _roleController;
 }
