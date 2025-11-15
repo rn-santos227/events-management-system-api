df51c4419c0ee8c35e00ce4037d73d811c45a8dc
rn-santos227 <rn_santos227@yahoo.com>
2025-11-15 17:51:01 +0800

    created sed user command cli file

---
+++ src/main/java/com/mgnt/events/cli/SeedUserCommand.java
@@ -0,0 +1,9 @@
+package com.mgnt.events.cli;
+
+public final class SeedUserCommand {
+  private SeedUserCommand() {}
+  
+  public static void main(String[] args) {
+    SeedUserOptions _options;
+  }
+}
