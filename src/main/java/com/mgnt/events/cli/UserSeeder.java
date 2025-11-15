9bcf27c1b964385ff8a9637247c713549e38205e
rn-santos227 <rn_santos227@yahoo.com>
2025-11-15 18:02:59 +0800

    added seed function in user seeder cli file

--- src/main/java/com/mgnt/events/cli/UserSeeder.java
+++ src/main/java/com/mgnt/events/cli/UserSeeder.java
@@ -1,8 +1,12 @@
 package com.mgnt.events.cli;
 
+import java.util.Optional;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.mgnt.events.models.User;
 import com.mgnt.events.repositories.RoleRepository;
 import com.mgnt.events.repositories.UserRepository;
 
@@ -20,5 +24,16 @@ public class UserSeeder {
     this._userRepository = userRepository;
     this._roleRepository = roleRepository;
     this._passwordEncoder = passwordEncoder;
+  }
+
+  @Transactional(rollbackFor = Throwable.class)
+  public void seed(SeedUserOptions options) {
+    String normalizedEmail = options.email();
+    Optional<User> _existingUser = _userRepository.findByEmail(normalizedEmail);
+    
+    if (_existingUser.isPresent() && !options.force()) {
+      System.out.printf("User %s already exists. Use --force to update.%n", normalizedEmail);
+      return;
+    }
   }
 }
