c30fbcf5eb394755f00efc5698fad21cf84c8931
rn-santos227 <rn_santos227@yahoo.com>
2025-11-16 18:26:17 +0800

    added main function in app key command cli file

--- src/main/java/com/mgnt/events/cli/AppKeyCommand.java
+++ src/main/java/com/mgnt/events/cli/AppKeyCommand.java
@@ -4,4 +4,8 @@ public final class AppKeyCommand {
   private static final String _DEFAULT_ENV_FILE = ".env.properties";
   
   private AppKeyCommand() {}
+
+  public static void main(String[] args) {
+
+  }
 }
