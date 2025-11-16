963922075946ae850431bd28448848e32b10a916
rn-santos227 <rn_santos227@yahoo.com>
2025-11-17 02:11:28 +0800

    updated login request file

--- src/main/java/com/mgnt/events/requests/auth/LoginRequest.java
+++ src/main/java/com/mgnt/events/requests/auth/LoginRequest.java
@@ -2,7 +2,11 @@ package com.mgnt.events.requests.auth;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class LoginRequest {
   @Email
   @NotBlank
@@ -10,20 +14,4 @@ public class LoginRequest {
 
   @NotBlank
   private String password;
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
 }
