f68ec056408c579d18a193b3e14e30f1ae01a928
rn-santos227 <rn_santos227@yahoo.com>
2025-11-21 18:44:07 +0800

    created audit log model file

--- src/main/java/com/mgnt/events/models/AuditLog.java
+++ src/main/java/com/mgnt/events/models/AuditLog.java
@@ -1,5 +1,16 @@
 package com.mgnt.events.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import com.mgnt.events.constants.Tables;
+
+@Entity
+@Table(name = Tables.AUDIT_LOGS)
+@Getter
+@Setter
 public class AuditLog {
     
 }
