5f15155fe73eedd95a1d2ccc67dfa16a1442c42d
rn-santos227 <rn_santos227@yahoo.com>
2025-11-21 19:52:47 +0800

    created audit log service file

--- src/main/java/com/mgnt/events/constants/Queries.java
+++ src/main/java/com/mgnt/events/constants/Queries.java
@@ -12,5 +12,6 @@ public class Queries {
   public static final String DELETE_STORED_FILES =
     "UPDATE stored_files SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?";
   public static final String LIMIT = "limit";
+  public static final String CREATED_AT = "createdAt";
   public static final String TEXT = "TEXT";
 }
