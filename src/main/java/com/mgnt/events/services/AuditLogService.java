346a1dd91a61ce55cf79620674efd3624e4517c6
rn-santos227 <rn_santos227@yahoo.com>
2025-11-21 19:59:28 +0800

    updated require user function in audit log service file

--- src/main/java/com/mgnt/events/services/AuditLogService.java
+++ src/main/java/com/mgnt/events/services/AuditLogService.java
@@ -1,17 +1,18 @@
 package com.mgnt.events.services;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.mgnt.events.constants.Queries;
 import com.mgnt.events.models.AuditLog;
 import com.mgnt.events.models.User;
 import com.mgnt.events.repositories.AuditLogRepository;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @Service
 public class AuditLogService {
@@ -45,6 +46,10 @@ public class AuditLogService {
   }
 
   private User requireUser() {
-
+    User user = resolveUser();
+    if (user == null) {
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Authentication is required");
+    }
+    return user;
   }
 }
