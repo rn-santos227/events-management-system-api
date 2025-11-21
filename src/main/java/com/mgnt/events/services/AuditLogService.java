4d0e31c6b83c34d95c13ccae370d5c5fc14c5462
rn-santos227 <rn_santos227@yahoo.com>
2025-11-21 19:53:41 +0800

    added constructor in audit log service file

--- src/main/java/com/mgnt/events/services/AuditLogService.java
+++ src/main/java/com/mgnt/events/services/AuditLogService.java
@@ -5,9 +5,15 @@ import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import com.mgnt.events.constants.Queries;
+import com.mgnt.events.repositories.AuditLogRepository;
 
 @Service
 public class AuditLogService {
   @NonNull
   private static final Sort DEFAULT_SORT = Sort.by(Sort.Direction.DESC, Queries.CREATED_AT);
+  private final AuditLogRepository _auditLogRepository;
+
+  public AuditLogService(AuditLogRepository auditLogRepository) {
+    this._auditLogRepository = auditLogRepository;
+  }
 }
