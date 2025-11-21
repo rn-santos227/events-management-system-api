bbd0c1dde6f31b686d6cbcb8f8fdcdc71d24ac76
rn-santos227 <rn_santos227@yahoo.com>
2025-11-21 18:45:06 +0800

    updated audit log model file

--- src/main/java/com/mgnt/events/models/AuditLog.java
+++ src/main/java/com/mgnt/events/models/AuditLog.java
@@ -1,13 +1,17 @@
 package com.mgnt.events.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+import com.mgnt.events.constants.Attributes;
 import com.mgnt.events.constants.Tables;
 
 @Entity
@@ -18,4 +22,8 @@ public class AuditLog {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = Attributes.USER_ID)
+  private User user;
 }
