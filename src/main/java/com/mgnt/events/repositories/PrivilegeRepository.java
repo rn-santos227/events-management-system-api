bacdb7aaa53803a8f27b7eb166b8597643b4acd8
rn-santos227 <rn_santos227@yahoo.com>
2025-11-23 00:52:34 +0800

    updated privilege repository file

--- src/main/java/com/mgnt/events/repositories/PrivilegeRepository.java
+++ src/main/java/com/mgnt/events/repositories/PrivilegeRepository.java
@@ -1,11 +1,12 @@
 package com.mgnt.events.repositories;
 
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mgnt.events.models.Privilege;
 
-public interface PrivilegeRepository extends JpaRepository<Privilege, Long> {
+public interface PrivilegeRepository extends JpaRepository<Privilege, UUID> {
   Optional<Privilege> findByNameIgnoreCase(String name);
   Optional<Privilege> findByActionIgnoreCase(String action);
 }
