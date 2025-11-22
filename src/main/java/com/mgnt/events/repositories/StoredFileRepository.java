2f3e7b06f32e407dc8fe3756b71be2639b2bd92f
rn-santos227 <rn_santos227@yahoo.com>
2025-11-23 00:53:52 +0800

    updated store file repository file

--- src/main/java/com/mgnt/events/repositories/StoredFileRepository.java
+++ src/main/java/com/mgnt/events/repositories/StoredFileRepository.java
@@ -1,7 +1,8 @@
 package com.mgnt.events.repositories;
 
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mgnt.events.models.StoredFile;
 
-public interface StoredFileRepository extends JpaRepository<StoredFile, Long> {}
+public interface StoredFileRepository extends JpaRepository<StoredFile, UUID> {}
