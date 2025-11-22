a5a22d86aaf6ee328bacc9fdcd11c9375b9c4c68
rn-santos227 <rn_santos227@yahoo.com>
2025-11-23 00:47:28 +0800

    updated role request file

--- src/main/java/com/mgnt/events/requests/roles/RoleRequest.java
+++ src/main/java/com/mgnt/events/requests/roles/RoleRequest.java
@@ -2,8 +2,9 @@ package com.mgnt.events.requests.roles;
 
 import jakarta.validation.constraints.NotBlank;
 import java.util.Set;
+import java.util.UUID;
 
 public record RoleRequest(
   @NotBlank(message = "Name is required") String name,
-  Set<Long> privilegeIds
+  Set<UUID> privilegeIds
 ) {}
--- src/main/java/com/mgnt/events/responses/roles/RoleSummary.java
+++ src/main/java/com/mgnt/events/responses/roles/RoleSummary.java
@@ -2,4 +2,4 @@ package com.mgnt.events.responses.roles;
 
 import java.util.UUID;
 
-public record RoleSummary(  UUID id,String name) {}
+public record RoleSummary(UUID id, String name) {}
