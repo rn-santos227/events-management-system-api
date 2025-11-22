3a1ff40f0664a541bdda3da001a8db3b0da2cd59
rn-santos227 <rn_santos227@yahoo.com>
2025-11-22 20:24:34 +0800

    added constructor in audit log controller file

--- src/main/java/com/mgnt/events/controllers/AuditLogController.java
+++ src/main/java/com/mgnt/events/controllers/AuditLogController.java
@@ -4,9 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mgnt.events.constants.Routes;
+import com.mgnt.events.services.AuditLogService;
 
 @RestController
 @RequestMapping(Routes.AUDIT_LOGS)
 public class AuditLogController {
-    
+  private final AuditLogService _auditLogService;
+
+  public AuditLogController(AuditLogService auditLogService) {
+    this._auditLogService = auditLogService;
+  }
 }
