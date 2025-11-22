46ba7df8ef4384cfc38aa7614035877b36dffc04
rn-santos227 <rn_santos227@yahoo.com>
2025-11-23 00:55:02 +0800

    updated user token repository file

--- src/main/java/com/mgnt/events/repositories/UserTokenRepository.java
+++ src/main/java/com/mgnt/events/repositories/UserTokenRepository.java
@@ -2,12 +2,13 @@ package com.mgnt.events.repositories;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mgnt.events.models.User;
 import com.mgnt.events.models.UserToken;
 
-public interface UserTokenRepository extends JpaRepository<UserToken, Long> {
+public interface UserTokenRepository extends JpaRepository<UserToken, UUID> {
   List<UserToken> findAllByUserAndExpiredFalseAndRevokedFalse(User user);
   Optional<UserToken> findByToken(String token);
 }
