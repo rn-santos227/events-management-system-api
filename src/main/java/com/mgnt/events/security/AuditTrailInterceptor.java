1f0704639409a2c0c5fe47c6763ff352326d97f0
rn-santos227 <rn_santos227@yahoo.com>
2025-11-22 19:42:11 +0800

    added after completion override function in audit trail interceptor security file

--- src/main/java/com/mgnt/events/security/AuditTrailInterceptor.java
+++ src/main/java/com/mgnt/events/security/AuditTrailInterceptor.java
@@ -1,8 +1,26 @@
 package com.mgnt.events.security;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.UrlPathHelper;
+
+import com.mgnt.events.services.AuditLogService;
 
 @Component
-public class AuditTrailInterceptor {
-    
+public class AuditTrailInterceptor extends HandlerInterceptor {
+
+
+  @Override
+  public void afterCompletion(
+    @NonNull HttpServletRequest request,
+    @NonNull HttpServletResponse response,
+    @NonNull Object handler,
+    Exception ex
+  ) throws Exception {
+
+  }    
 }
