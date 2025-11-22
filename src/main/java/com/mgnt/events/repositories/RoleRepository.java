7aa7582554eb06817c9f87f93f5f87afaf4a511e
rn-santos227 <rn_santos227@yahoo.com>
2025-11-23 00:53:11 +0800

    updated role repository file

--- src/main/java/com/mgnt/events/repositories/RoleRepository.java
+++ src/main/java/com/mgnt/events/repositories/RoleRepository.java
@@ -1,15 +1,16 @@
 package com.mgnt.events.repositories;
 
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mgnt.events.constants.Tables;
 import com.mgnt.events.models.Role;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, UUID> {
   Optional<Role> findByNameIgnoreCase(String name);
 
   @EntityGraph(attributePaths = Tables.PRIVILEGES)
-  Optional<Role> findWithPrivilegesById(Long id);
+  Optional<Role> findWithPrivilegesById(UUID id);
 }
