fe93c5afbd4f98cd23aaf49d8e5897bd7a3ddcd1
rn-santos227 <rn_santos227@yahoo.com>
2025-11-22 20:28:18 +0800

    added find all function in audit log controller file

--- src/main/java/com/mgnt/events/controllers/AuditLogController.java
+++ src/main/java/com/mgnt/events/controllers/AuditLogController.java
@@ -1,9 +1,16 @@
 package com.mgnt.events.controllers;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mgnt.events.constants.PrivilegeActions;
+import com.mgnt.events.constants.Queries;
 import com.mgnt.events.constants.Routes;
+import com.mgnt.events.responses.audit.AuditLogResponse;
+import com.mgnt.events.security.annotations.RequiresPrivilege;
 import com.mgnt.events.services.AuditLogService;
 
 @RestController
@@ -13,5 +20,11 @@ public class AuditLogController {
 
   public AuditLogController(AuditLogService auditLogService) {
     this._auditLogService = auditLogService;
+  }
+
+  @GetMapping
+  @RequiresPrivilege({ PrivilegeActions.AUDIT_LOGS_READ })
+  public List<AuditLogResponse> findAll(@RequestParam(name = Queries.LIMIT, required = false) Integer limit) {
+
   }
 }
