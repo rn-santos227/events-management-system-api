ea57715e1968b009575f033b0dfacdacee601477
rn-santos227 <rn_santos227@yahoo.com>
2025-11-22 19:43:48 +0800

    updated audit trail interceptor security file

--- src/main/java/com/mgnt/events/security/AuditTrailInterceptor.java
+++ src/main/java/com/mgnt/events/security/AuditTrailInterceptor.java
@@ -4,16 +4,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.util.UrlPathHelper;
 
 import com.mgnt.events.services.AuditLogService;
 
 @Component
-public class AuditTrailInterceptor extends HandlerInterceptor {
+public class AuditTrailInterceptor implements  HandlerInterceptor {
   private final AuditLogService _auditLogService;
   private final UrlPathHelper _urlPathHelper = new UrlPathHelper();
+
+  public AuditTrailInterceptor(AuditLogService auditLogService) {
+    this._auditLogService = auditLogService;
+  }
 
   @Override
   public void afterCompletion(
