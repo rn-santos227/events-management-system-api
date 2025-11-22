7ea8ef481c78902b0c3b9f49c8f552242f19845d
rn-santos227 <rn_santos227@yahoo.com>
2025-11-23 00:48:13 +0800

    updated user create request file

--- src/main/java/com/mgnt/events/requests/users/UserCreateRequest.java
+++ src/main/java/com/mgnt/events/requests/users/UserCreateRequest.java
@@ -3,6 +3,7 @@ package com.mgnt.events.requests.users;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
 
 public record UserCreateRequest(
   @NotBlank(message = "Email is required") @Email(message = "Email must be valid") String email,
@@ -10,6 +11,6 @@ public record UserCreateRequest(
   @NotBlank(message = "First name is required") String firstName,
   @NotBlank(message = "Last name is required") String lastName,
   @NotBlank(message = "Contact number is required") String contactNumber,
-  @NotNull(message = "Role is required") Long roleId,
+  @NotNull(message = "Role is required") UUID roleId,
   Boolean active
 ) {}
