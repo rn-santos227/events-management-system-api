e227e08eeecd38e9f282ce4e1f95fd45c4e43900
rn-santos227 <rn_santos227@yahoo.com>
2025-11-23 00:54:33 +0800

    updated user repository file

--- src/main/java/com/mgnt/events/repositories/UserRepository.java
+++ src/main/java/com/mgnt/events/repositories/UserRepository.java
@@ -1,12 +1,13 @@
 package com.mgnt.events.repositories;
 
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mgnt.events.models.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
   Optional<User> findByEmail(String email);
   boolean existsByEmail(String email);
-  boolean existsByEmailAndIdNot(String email, Long id);
+  boolean existsByEmailAndIdNot(String email, UUID id);
 }
