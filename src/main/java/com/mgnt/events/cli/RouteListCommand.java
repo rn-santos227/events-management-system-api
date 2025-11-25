813374727c7c58b5947cd72260e2654f1db50a66
rn-santos227 <rn_santos227@yahoo.com>
2025-11-25 17:30:31 +0800

    added print rest routes in route list command cli file

--- src/main/java/com/mgnt/events/cli/RouteListCommand.java
+++ src/main/java/com/mgnt/events/cli/RouteListCommand.java
@@ -1,9 +1,15 @@
 package com.mgnt.events.cli;
 
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
 public class RouteListCommand {
   private RouteListCommand() {}
 
   public static void main(String[] args) {
+
+  }
+
+  private static void printRestRoutes(RequestMappingHandlerMapping mapping) {
 
   }
 }
