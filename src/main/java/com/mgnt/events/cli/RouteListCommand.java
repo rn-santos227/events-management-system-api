56cdc03c9f6568772da35a08df4d9d4380caeb19
rn-santos227 <rn_santos227@yahoo.com>
2025-11-25 17:32:16 +0800

    added print graphql routes in route list command cli file

--- src/main/java/com/mgnt/events/cli/RouteListCommand.java
+++ src/main/java/com/mgnt/events/cli/RouteListCommand.java
@@ -1,5 +1,8 @@
 package com.mgnt.events.cli;
 
+import java.util.Objects;
+
+import org.springframework.graphql.execution.GraphQlSource;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 public class RouteListCommand {
@@ -10,6 +13,10 @@ public class RouteListCommand {
   }
 
   private static void printRestRoutes(RequestMappingHandlerMapping mapping) {
+    Objects.requireNonNull(mapping, "Request mapping handler must not be null");
+  }
+
+  private static void printGraphqlRoutes(GraphQlSource graphQlSource) {
 
   }
 }
