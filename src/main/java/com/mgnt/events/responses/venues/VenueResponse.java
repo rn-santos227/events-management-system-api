5c2d872b5b92a2fba1e9adc2229ebcc3222b6135
rn-santos227 <rn_santos227@yahoo.com>
2025-11-27 12:00:00 +0800

    created venue response file

---
+++ src/main/java/com/mgnt/events/responses/venues/VenueResponse.java
@@ -0,0 +1,13 @@
+package com.mgnt.events.responses.venues;
+
+import com.mgnt.events.enums.VenueType;
+import com.mgnt.events.responses.files.StoredFileSummary;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record VenueResponse(
+  UUID id,
+  String name
+) {}
