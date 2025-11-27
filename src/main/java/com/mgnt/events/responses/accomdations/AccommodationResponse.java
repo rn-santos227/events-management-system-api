7a88c7e097fb807b8e3762bb4fc60a207de73458
rn-santos227 <rn_santos227@yahoo.com>
2025-11-27 12:00:00 +0800

    created accommodation response file

---
+++ src/main/java/com/mgnt/events/responses/accomdations/AccommodationResponse.java
@@ -0,0 +1,12 @@
+package com.mgnt.events.responses.accomdations;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.mgnt.events.enums.AccommodationType;
+import com.mgnt.events.responses.files.StoredFileSummary;
+
+public record AccommodationResponse(
+
+) {}
