00ef7259d5d023dfe255df37dce83dbd3a85a8fa
rn-santos227 <rn_santos227@yahoo.com>
2025-11-28 23:37:41 +0800

    updated create general tables migration file

--- src/main/resources/db/V3__create_general_tables.sql
+++ src/main/resources/db/V3__create_general_tables.sql
@@ -16,3 +16,16 @@ CREATE TABLE personnel (
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   deleted_at TIMESTAMP
 );
+
+CREATE TABLE vehicles (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name VARCHAR(255) NOT NULL,
+  type VARCHAR(50) NOT NULL,
+  status VARCHAR(50) NOT NULL,
+  contact_number VARCHAR(20) NOT NULL,
+  personnel_id UUID,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP,
+  CONSTRAINT fk_vehicles_personnel FOREIGN KEY (personnel_id) REFERENCES personnel (id)
+);
