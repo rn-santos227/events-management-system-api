3f4efbd7e326c26e3b6e0d8f1a7d74ad9380eca5
rn-santos227 <rn_santos227@yahoo.com>
2025-11-28 23:37:54 +0800

    updated create general tables migration file

--- src/main/resources/db/V3__create_general_tables.sql
+++ src/main/resources/db/V3__create_general_tables.sql
@@ -29,3 +29,19 @@ CREATE TABLE vehicles (
   deleted_at TIMESTAMP,
   CONSTRAINT fk_vehicles_personnel FOREIGN KEY (personnel_id) REFERENCES personnel (id)
 );
+
+CREATE TABLE venues (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name VARCHAR(255) NOT NULL,
+  address TEXT NOT NULL,
+  contact_person VARCHAR(255) NOT NULL,
+  contact_number VARCHAR(20),
+  type VARCHAR(50) NOT NULL,
+  latitude NUMERIC(9,6),
+  longitude NUMERIC(9,6),
+  image_id UUID,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP,
+  CONSTRAINT fk_venues_image FOREIGN KEY (image_id) REFERENCES stored_files (id)
+);
