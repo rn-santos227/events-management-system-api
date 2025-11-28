3a0e71ac50d688756bedc2a4f84c80797330e582
rn-santos227 <rn_santos227@yahoo.com>
2025-11-28 23:43:16 +0800

    created personnel model file

---
+++ src/main/java/com/mgnt/events/models/Personnel.java
@@ -0,0 +1,22 @@
+package com.mgnt.events.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import com.mgnt.events.constants.Attributes;
+import com.mgnt.events.constants.Defaults;
+import com.mgnt.events.constants.Queries;
+import com.mgnt.events.constants.Tables;
+
+public class Personnel extends AuditableEntity {
+
+}
