fec77d3afed0c27f5bfad8d980b0ca7a99f9aeb0
rn-santos227 <rn_santos227@yahoo.com>
2025-11-28 23:41:45 +0800

    updated routes constants file

--- src/main/java/com/mgnt/events/constants/Routes.java
+++ src/main/java/com/mgnt/events/constants/Routes.java
@@ -23,6 +23,8 @@ public class Routes {
   public static final String ROLES = BASE_PATH + "/roles";
   public static final String USERS = BASE_PATH + "/users";
   public static final String AUDIT_LOGS = BASE_PATH + "/audit-logs";
+  public static final String VEHICLES = BASE_PATH + "/vehicles";
+  public static final String VENUES = BASE_PATH + "/venues";
 
   public static final String APPEND_ID = "/{id}";
   public static final String APPEND_USER_ID = "/users" + APPEND_ID;
