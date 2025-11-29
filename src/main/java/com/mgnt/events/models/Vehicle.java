93d7ef7cdb08198fe8b4573a4d21874ed7a1ef9b
rn-santos227 <rn_santos227@yahoo.com>
2025-11-29 22:25:12 +0800

    created vehicle model file

---
+++ src/main/java/com/mgnt/events/models/Vehicle.java
@@ -0,0 +1,22 @@
+package com.mgnt.events.models;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import com.mgnt.events.constants.Queries;
+import com.mgnt.events.constants.Tables;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = Tables.VEHICLES)
+@SQLDelete(sql = Queries.DELETE_VEHICLES)
+@SQLRestriction(Queries.DELETE_RESTRICTION)
+@Getter
+@Setter
+public class Vehicle extends AuditableEntity {
+  
+}
