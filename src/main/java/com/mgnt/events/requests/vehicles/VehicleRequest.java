793be235d76284c356fd9543d9cf644eac8b7f0d
rn-santos227 <rn_santos227@yahoo.com>
2025-11-30 23:06:22 +0800

    updated vehicle request file

--- src/main/java/com/mgnt/events/requests/vehicles/VehicleRequest.java
+++ src/main/java/com/mgnt/events/requests/vehicles/VehicleRequest.java
@@ -11,5 +11,8 @@ import com.mgnt.events.enums.VehicleType;
 
 public record VehicleRequest(
   @NotBlank(message = "Name is required") @Size(max = Defaults.DEFAULT_MAX_STRING_LENGTH, message = "Name is too long") String name,
-  @NotNull(message = "Type is required") VehicleType type
+  @NotNull(message = "Type is required") VehicleType type,
+  @NotNull(message = "Status is required") VehicleStatus status,
+  @NotBlank(message = "Plate number is required") @Size(max = 50, message = "Plate number is too long") String plateNumber,
+  @NotBlank(message = "Contact number is required") @Size(max = 20, message = "Contact number is too long") String contactNumber
 ) {}
