ec099a1555f235b2c5fd59d1cbea05c68a67a955
rn-santos227 <rn_santos227@yahoo.com>
2025-11-30 23:14:47 +0800

    updated venue request file

--- src/main/java/com/mgnt/events/requests/venues/VenueRequest.java
+++ src/main/java/com/mgnt/events/requests/venues/VenueRequest.java
@@ -1,12 +1,15 @@
 package com.mgnt.events.requests.venues;
 
-import com.mgnt.events.constants.Defaults;
-
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import com.mgnt.events.constants.Defaults;
 
 public record VenueRequest(
   @NotBlank(message = "Name is required") @Size(max = Defaults.DEFAULT_MAX_STRING_LENGTH, message = "Name is too long") String name,
   @NotBlank(message = "Address is required") String address,
-  @NotBlank(message = "Contact person is required") @Size(max = Defaults.DEFAULT_MAX_STRING_LENGTH, message = "Contact person is too long") String contactPerson
+  @NotBlank(message = "Contact person is required") @Size(max = Defaults.DEFAULT_MAX_STRING_LENGTH, message = "Contact person is too long") String contactPerson,
+  @Size(max = Defaults.DEFAULT_PHONE_LENGTH, message = "Contact number is too long") String contactNumber,
+  @Email(message = "Invalid email") @Size(max = 100, message = "Email is too long") String email
 ) {}
