078c32cd80c2e4fb70ad63ee43dd23043fbd4601
rn-santos227 <rn_santos227@yahoo.com>
2025-11-30 23:02:59 +0800

    updated personnel request file

--- src/main/java/com/mgnt/events/requests/personnel/PersonnelRequest.java
+++ src/main/java/com/mgnt/events/requests/personnel/PersonnelRequest.java
@@ -1,9 +1,11 @@
 package com.mgnt.events.requests.personnel;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record  PersonnelRequest(
   @NotBlank(message = "Name is required") @Size(max = 255, message = "Name is too long") String name,
-  @NotBlank(message = "Contact number is required") @Size(max = 20, message = "Contact number is too long") String contactNumber
+  @NotBlank(message = "Contact number is required") @Size(max = 20, message = "Contact number is too long") String contactNumber,
+  @Email(message = "Invalid email") @Size(max = 100, message = "Email is too long") String email
 ) {}
