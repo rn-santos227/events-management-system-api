72c576c1889c03ed228bff6a7aff024b35933a61
rn-santos227 <rn_santos227@yahoo.com>
2025-11-30 22:59:46 +0800

    created category request file

---
+++ src/main/java/com/mgnt/events/requests/categories/CategoryRequest.java
@@ -0,0 +1,8 @@
+package com.mgnt.events.requests.categories;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CategoryRequest (
+  
+) {}
