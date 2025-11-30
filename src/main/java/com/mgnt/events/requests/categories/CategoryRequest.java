3c7db1ded929442cb34087dee79cfd99241c0db7
rn-santos227 <rn_santos227@yahoo.com>
2025-11-30 23:00:21 +0800

    updated category request file

--- src/main/java/com/mgnt/events/requests/categories/CategoryRequest.java
+++ src/main/java/com/mgnt/events/requests/categories/CategoryRequest.java
@@ -4,5 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CategoryRequest (
-  @NotBlank(message = "Name is required") @Size(max = 255, message = "Name is too long") String name
+  @NotBlank(message = "Name is required") @Size(max = 255, message = "Name is too long") String name,
+  @Size(max = 1024, message = "Description is too long") String description
 ) {}
