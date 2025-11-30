acdfd1990cd5b9a6e32dc3d540bab56e44d19e04
rn-santos227 <rn_santos227@yahoo.com>
2025-11-30 23:05:11 +0800

    updated vehicle request file

--- src/main/java/com/mgnt/events/requests/vehicles/VehicleRequest.java
+++ src/main/java/com/mgnt/events/requests/vehicles/VehicleRequest.java
@@ -1,5 +1,13 @@
 package com.mgnt.events.requests.vehicles;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.UUID;
+
+import com.mgnt.events.enums.VehicleStatus;
+import com.mgnt.events.enums.VehicleType;
+
 public record VehicleRequest(
-  
+  @NotBlank(message = "Name is required") @Size(max = 255, message = "Name is too long") String name
 ) {}
