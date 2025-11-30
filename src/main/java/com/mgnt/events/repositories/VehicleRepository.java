c9dad968bea881eaa0e86c09f25f1f4c0c3c9733
rn-santos227 <rn_santos227@yahoo.com>
2025-11-30 22:55:46 +0800

    created vehicle repository file

---
+++ src/main/java/com/mgnt/events/repositories/VehicleRepository.java
@@ -0,0 +1,10 @@
+package com.mgnt.events.repositories;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mgnt.events.models.Vehicle;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {}
