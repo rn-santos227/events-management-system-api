e7b02d350440a8c8bc3ef0782820a440fbc148d1
rn-santos227 <rn_santos227@yahoo.com>
2025-12-01 20:05:15 +0800

    added find all function in category service file

--- src/main/java/com/mgnt/events/services/CategoryService.java
+++ src/main/java/com/mgnt/events/services/CategoryService.java
@@ -1,15 +1,21 @@
 package com.mgnt.events.services;
 
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mgnt.events.constants.Attributes;
+import com.mgnt.events.constants.Queries;
 import com.mgnt.events.models.Category;
 import com.mgnt.events.repositories.CategoryRepository;
 import com.mgnt.events.responses.categories.CategoryResponse;
+import com.mgnt.events.util.RequestValidators;
+
+import jakarta.annotation.Nullable;
 
 @Service
 public class CategoryService {
@@ -19,6 +25,11 @@ public class CategoryService {
 
   public CategoryService(CategoryRepository categoryService) {
     this._categoryRepository = categoryService;
+  }
+
+  @Transactional(readOnly = true)
+  public List<CategoryResponse> findAll(@Nullable Integer limit) {
+    Integer sanitizedLimit = RequestValidators.requirePositiveOrNull(limit, Queries.LIMIT);
   }
   
   private CategoryResponse toRespose(Category category) {
