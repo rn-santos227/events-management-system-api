1e84c6a36c64eaa464d94adf722e4d666df7b327
rn-santos227 <rn_santos227@yahoo.com>
2025-12-01 19:42:53 +0800

    updated accommodation request file

--- src/main/java/com/mgnt/events/requests/accommodations/AccommodationRequest.java
+++ src/main/java/com/mgnt/events/requests/accommodations/AccommodationRequest.java
@@ -1,9 +1,13 @@
 package com.mgnt.events.requests.accommodations;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
 
 import com.mgnt.events.constants.Defaults;
 import com.mgnt.events.enums.AccommodationType;
@@ -14,5 +18,8 @@ public record AccommodationRequest(
   @NotBlank(message = "Contact person is required") @Size(max = Defaults.DEFAULT_MAX_STRING_LENGTH, message = "Contact person is too long") String contactPerson,
   @Size(max = Defaults.DEFAULT_CONTENT_TYPE_LENGTH, message = "Contact number is too long") String contactNumber,
   @Email(message = "Invalid email") @Size(max = Defaults.DEFAULT_EMAIL_LENGTH, message = "Email is too long") String email,
-  @NotNull(message = "Type is required") AccommodationType type
+  @NotNull(message = "Type is required") AccommodationType type,
+  @DecimalMin(value = Defaults.DEG_90_NEGATIVE, message = "Latitude must be greater than or equal to -90")
+  @DecimalMax(value = Defaults.DEG_90_POSITIVE, message = "Latitude must be less than or equal to 90")
+  BigDecimal latitude
 ) {}
