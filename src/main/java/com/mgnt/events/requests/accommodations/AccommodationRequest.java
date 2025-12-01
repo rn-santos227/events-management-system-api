7f3e3a0acfa84029419df747f1e056ee4b9ed45e
rn-santos227 <rn_santos227@yahoo.com>
2025-12-01 19:41:32 +0800

    updated accommodation request file

--- src/main/java/com/mgnt/events/requests/accommodations/AccommodationRequest.java
+++ src/main/java/com/mgnt/events/requests/accommodations/AccommodationRequest.java
@@ -8,5 +8,6 @@ import jakarta.validation.constraints.Size;
 public record AccommodationRequest(
   @NotBlank(message = "Name is required") @Size(max = Defaults.DEFAULT_MAX_STRING_LENGTH, message = "Name is too long") String name,
   @NotBlank(message = "Address is required") String address,
-  @NotBlank(message = "Contact person is required") @Size(max = Defaults.DEFAULT_MAX_STRING_LENGTH, message = "Contact person is too long") String contactPerson
+  @NotBlank(message = "Contact person is required") @Size(max = Defaults.DEFAULT_MAX_STRING_LENGTH, message = "Contact person is too long") String contactPerson,
+  @Size(max = Defaults.DEFAULT_CONTENT_TYPE_LENGTH, message = "Contact number is too long") String contactNumber
 ) {}
