b156593f01a831ee0f7772f9d34458e3cee20f05
rn-santos227 <rn_santos227@yahoo.com>
2025-12-05 18:35:57 +0800

    added constructor in vehicle service file

--- src/main/java/com/mgnt/events/services/VehicleService.java
+++ src/main/java/com/mgnt/events/services/VehicleService.java
@@ -5,9 +5,19 @@ import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import com.mgnt.events.constants.Attributes;
+import com.mgnt.events.repositories.PersonnelRepository;
+import com.mgnt.events.repositories.VehicleRepository;
 
 @Service
 public class VehicleService {
   @NonNull
   private static final Sort DEFAULT_SORT = Sort.by(Sort.Direction.ASC, Attributes.NAME);
+
+  private final VehicleRepository _vehicleRepository;
+  private final PersonnelRepository _personnelRepository;
+
+  public VehicleService(VehicleRepository vehicleRepository, PersonnelRepository personnelRepository) {
+    this._vehicleRepository = vehicleRepository;
+    this._personnelRepository = personnelRepository;
+  }
 }
