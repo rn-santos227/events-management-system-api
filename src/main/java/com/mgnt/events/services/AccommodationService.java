8efc414fd3a0edf19d492efe724ff460db234ced
rn-santos227 <rn_santos227@yahoo.com>
2025-12-10 20:30:09 +0800

    added constructor in accommodation service file

--- src/main/java/com/mgnt/events/services/AccommodationService.java
+++ src/main/java/com/mgnt/events/services/AccommodationService.java
@@ -5,9 +5,22 @@ import org.springframework.stereotype.Service;
 import org.springframework.lang.NonNull;
 
 import com.mgnt.events.constants.Attributes;
+import com.mgnt.events.repositories.AccommodationRepository;
+import com.mgnt.events.repositories.StoredFileRepository;
 
 @Service
 public class AccommodationService {
   @NonNull
   private static final Sort DEFAULT_SORT = Sort.by(Sort.Direction.ASC, Attributes.NAME);
+
+  private final AccommodationRepository _accommodationRepository;
+  private final StoredFileRepository _storedFileRepository;
+
+  public AccommodationService(
+    AccommodationRepository accommodationRepository,
+    StoredFileRepository storedFileRepository
+  ) {
+    this._accommodationRepository = accommodationRepository;
+    this._storedFileRepository = storedFileRepository;
+  }
 }
