80b478daef0427dfd960c9607ed5fd6d385addc1
rn-santos227 <rn_santos227@yahoo.com>
2025-12-11 22:01:20 +0800

    added constructor in personnel controller file

--- src/main/java/com/mgnt/events/controllers/PersonnelController.java
+++ src/main/java/com/mgnt/events/controllers/PersonnelController.java
@@ -4,9 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mgnt.events.constants.Routes;
+import com.mgnt.events.services.PersonnelService;
 
 @RestController
 @RequestMapping(Routes.PERSONNEL)
 public class PersonnelController {
-  
+ private final PersonnelService _personnelService;
+
+  public PersonnelController(PersonnelService personnelService) {
+    this._personnelService = personnelService;
+  }
 }
