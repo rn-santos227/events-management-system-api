64e6f9742245058c1adebb5efbd6bb00182192e2
rn-santos227 <rn_santos227@yahoo.com>
2025-12-11 21:53:38 +0800

    added constructor in categories controller file

--- src/main/java/com/mgnt/events/controllers/CategoryController.java
+++ src/main/java/com/mgnt/events/controllers/CategoryController.java
@@ -4,9 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mgnt.events.constants.Routes;
+import com.mgnt.events.services.CategoryService;
 
 @RestController
 @RequestMapping(Routes.CATEGORIES)
 public class CategoryController {
-  
+  private final CategoryService _categoryService ;
+
+  public CategoryController(CategoryService categoryService) {
+    this._categoryService = categoryService;
+  }
 }
