c4986029448082e7f5f5bab4d5ccafad88bebfcd
rn-santos227 <rn_santos227@yahoo.com>
2025-12-11 21:58:15 +0800

    added update function in categories controller file

--- src/main/java/com/mgnt/events/controllers/CategoryController.java
+++ src/main/java/com/mgnt/events/controllers/CategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,5 +50,10 @@ public class CategoryController {
   @ResponseStatus(HttpStatus.CREATED)
   public CategoryResponse create(@Valid @RequestBody CategoryRequest request) {
     return _categoryService.create(request);
+  }
+
+  @PutMapping(Routes.APPEND_ID)
+  public CategoryResponse update(@PathVariable @NonNull UUID id, @Valid @RequestBody CategoryRequest request) {
+    return _categoryService.update(id, request);
   }
 }
