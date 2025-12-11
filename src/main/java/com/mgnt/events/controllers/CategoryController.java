d98570a4a99cbbdb5caf435e08be4c4c8cd02966
rn-santos227 <rn_santos227@yahoo.com>
2025-12-11 21:55:21 +0800

    added find all function in categories controller file

--- src/main/java/com/mgnt/events/controllers/CategoryController.java
+++ src/main/java/com/mgnt/events/controllers/CategoryController.java
@@ -1,10 +1,17 @@
 package com.mgnt.events.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mgnt.events.constants.Queries;
 import com.mgnt.events.constants.Routes;
+import com.mgnt.events.responses.categories.CategoryResponse;
 import com.mgnt.events.services.CategoryService;
+import com.mgnt.events.util.RequestValidators;
 
 @RestController
 @RequestMapping(Routes.CATEGORIES)
@@ -13,5 +20,13 @@ public class CategoryController {
 
   public CategoryController(CategoryService categoryService) {
     this._categoryService = categoryService;
+  }
+
+  @GetMapping
+  public List<CategoryResponse> findAll(
+    @RequestParam(name = Queries.LIMIT, required = false) Integer limit
+  ) {
+    Integer sanitizedLimit = RequestValidators.requirePositiveOrNull(limit, Queries.LIMIT);
+    return _categoryService.findAll(sanitizedLimit);
   }
 }
