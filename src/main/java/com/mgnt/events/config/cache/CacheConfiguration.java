4eac16ff29a14afbf1bbc64b5ccff43db695f482
rn-santos227 <rn_santos227@yahoo.com>
2025-12-12 21:10:35 +0800

    added cache maanger constructor in cache configuration file

--- src/main/java/com/mgnt/events/config/cache/CacheConfiguration.java
+++ src/main/java/com/mgnt/events/config/cache/CacheConfiguration.java
@@ -1,10 +1,22 @@
 package com.mgnt.events.config.cache;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import com.mgnt.events.constants.Config;
 
 @Configuration
 @EnableCaching
 public class CacheConfiguration {
+  @Bean
+  public CacheManager cacheManager(
+    RedisConnectionFactory connectionFactory,
+    @Value(Config.CACHE_TTL) long timeToLiveMillis
+  ) {
 
+  }
 }
