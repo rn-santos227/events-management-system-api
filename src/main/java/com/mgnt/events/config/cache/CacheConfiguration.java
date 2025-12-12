971ce51a63ebbd21f82df1f4b120633556ce2865
rn-santos227 <rn_santos227@yahoo.com>
2025-12-12 20:42:48 +0800

    created cache configuration fle

---
+++ src/main/java/com/mgnt/events/config/cache/CacheConfiguration.java
@@ -0,0 +1,10 @@
+package com.mgnt.events.config.cache;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+  
+}
