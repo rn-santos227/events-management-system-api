f3537daee4bde91804992f74e78eb21a3e1874a2
rn-santos227 <rn_santos227@yahoo.com>
2025-12-12 20:40:10 +0800

    created cache constant file

---
+++ src/main/java/com/mgnt/events/constants/Cache.java
@@ -0,0 +1,16 @@
+package com.mgnt.events.constants;
+
+public final class Cache {
+  private Cache() {}
+
+  public static final String ACCOMMODATIONS = "accommodations";
+  public static final String ACCOMMODATION_BY_ID = "accommodation-by-id";
+  public static final String CATEGORIES = "categories";
+  public static final String CATEGORY_BY_ID = "category-by-id";
+  public static final String PERSONNEL = "personnel";
+  public static final String PERSONNEL_BY_ID = "personnel-by-id";
+  public static final String VEHICLES = "vehicles";
+  public static final String VEHICLE_BY_ID = "vehicle-by-id";
+  public static final String VENUES = "venues";
+  public static final String VENUE_BY_ID = "venue-by-id";
+}
