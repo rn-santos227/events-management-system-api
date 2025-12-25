2b56faf221e0eed5ae4827b96336b48d7a331fc1
rn-santos227 <rn_santos227@yahoo.com>
2025-12-25 21:06:12 +0800

    updated cache constant file

--- src/main/java/com/mgnt/events/constants/Cache.java
+++ src/main/java/com/mgnt/events/constants/Cache.java
@@ -11,6 +11,10 @@ public final class Cache {
   public static final String CATEGORY_BY_ID = "category-by-id";
   public static final String PERSONNEL = "personnel";
   public static final String PERSONNEL_BY_ID = "personnel-by-id";
+  public static final String PRIVILEGES = "privileges";
+  public static final String PRIVILEGE_BY_ID = "privilege-by-id";
+  public static final String ROLES = "roles";
+  public static final String ROLE_BY_ID = "role-by-id";
   public static final String USERS = "users";
   public static final String USER_BY_ID = "user-by-id";
   public static final String VEHICLES = "vehicles";
@@ -20,5 +24,6 @@ public final class Cache {
 
   public static final String KEY = "T(String).format('%s:%s', #limit?:'all', #page?:'all')";
   public static final String KEY_ALL = "#limit?:'all'";
+  public static final String KEY_ID = "#id";
   public static final String KEY_USER = "T(String).format('%s:%s', T(java.util.Objects).requireNonNull(#root.target.requireUser()).getId(), #limit?:'all')";
 }
