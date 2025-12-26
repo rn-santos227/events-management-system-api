e7ed26a228ed308aae7e4899bd099af496872c5b
rn-santos227 <rn_santos227@yahoo.com>
2025-12-26 20:52:56 +0800

    added setup function in personnel controller test file

--- src/test/java/com/mgnt/events/controllers/PersonnelControllerTest.java
+++ src/test/java/com/mgnt/events/controllers/PersonnelControllerTest.java
@@ -1,5 +1,6 @@
 package com.mgnt.events.controllers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -7,6 +8,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.mgnt.events.services.PersonnelService;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,4 +21,11 @@ public class PersonnelControllerTest {
 
   @InjectMocks
   private PersonnelController _personnelController;
+
+  @BeforeEach
+  void setup() {
+    _objectMapper = new ObjectMapper();
+    _objectMapper.registerModule(new JavaTimeModule());
+
+  }
 }
