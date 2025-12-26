570c7d05acd4f8b4265df33841c9c53ed96a39a3
rn-santos227 <rn_santos227@yahoo.com>
2025-12-26 20:39:58 +0800

    created category controller test file

---
+++ src/test/java/com/mgnt/events/controllers/CategoryControllerTest.java
@@ -0,0 +1,10 @@
+package com.mgnt.events.controllers;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class CategoryControllerTest {
+  private MockMvc _mockMvc;
+  private ObjectMapper _objectMapper;
+}
