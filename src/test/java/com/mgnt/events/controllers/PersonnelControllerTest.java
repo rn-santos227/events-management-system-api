3bd3abc7debade8ee395698df299a626f47c4d68
rn-santos227 <rn_santos227@yahoo.com>
2025-12-26 20:51:54 +0800

    updated personnel controller test file

--- src/test/java/com/mgnt/events/controllers/PersonnelControllerTest.java
+++ src/test/java/com/mgnt/events/controllers/PersonnelControllerTest.java
@@ -1,14 +1,22 @@
 package com.mgnt.events.controllers;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mgnt.events.services.PersonnelService;
 
 @ExtendWith(MockitoExtension.class)
 public class PersonnelControllerTest {
   private MockMvc _mockMvc;
   private ObjectMapper _objectMapper;
 
+  @Mock
+  private PersonnelService _personnelService;
+
+  @InjectMocks
+  private PersonnelController _personnelController;
 }
