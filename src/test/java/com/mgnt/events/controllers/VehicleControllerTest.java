faef915f7e638fb808839c061b405a2e7e1c1011
rn-santos227 <rn_santos227@yahoo.com>
2025-12-27 02:01:17 +0800

    created vehicle controller test file

---
+++ src/test/java/com/mgnt/events/controllers/VehicleControllerTest.java
@@ -0,0 +1,9 @@
+package com.mgnt.events.controllers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class VehicleControllerTest {
+  
+}
