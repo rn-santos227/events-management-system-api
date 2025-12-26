e502ddecc54012184bf1550f3b6e6ddbe3bc4740
rn-santos227 <rn_santos227@yahoo.com>
2025-12-27 02:03:40 +0800

    updated vehicle controller test file

---
+++ src/test/java/com/mgnt/events/controllers/VehicleControllerTest.java
@@ -0,0 +1,23 @@
+package com.mgnt.events.controllers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mgnt.events.services.VehicleService;
+
+@ExtendWith(MockitoExtension.class)
+public class VehicleControllerTest {
+  private MockMvc _mockMvc;
+  private ObjectMapper _objectMapper;
+
+  @Mock
+  private VehicleService _vehicleService;
+
+  @InjectMocks
+  private VehicleController _vehicleController;
+
+}
