f87c68403b0bad48d68d3eaedbdc04600570890f
rn-santos227 <rn_santos227@yahoo.com>
2025-12-27 17:32:24 +0800

    updated venue controller test file

--- src/test/java/com/mgnt/events/controllers/VenueControllerTest.java
+++ src/test/java/com/mgnt/events/controllers/VenueControllerTest.java
@@ -1,13 +1,23 @@
 package com.mgnt.events.controllers;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mgnt.events.services.VenueService;
 
 @ExtendWith(MockitoExtension.class)
 public class VenueControllerTest {
   private MockMvc _mockMvc;
   private ObjectMapper _objectMapper;
+
+  @Mock
+  private VenueService _venueService;
+
+  @InjectMocks
+  private VenueController _venueController;
+
 }
