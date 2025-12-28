5119c38d9f62025b96f06eb275b78b2d5d27aa3d
rn-santos227 <rn_santos227@yahoo.com>
2025-12-28 18:51:22 +0800

    updated accommodation controller test file

--- src/test/java/com/mgnt/events/controllers/AccommodationControllerTest.java
+++ src/test/java/com/mgnt/events/controllers/AccommodationControllerTest.java
@@ -2,8 +2,12 @@ package com.mgnt.events.controllers;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(MockitoExtension.class)
 public class AccommodationControllerTest {
-
+  private MockMvc _mockMvc;
+  private ObjectMapper _objectMapper;
 }
