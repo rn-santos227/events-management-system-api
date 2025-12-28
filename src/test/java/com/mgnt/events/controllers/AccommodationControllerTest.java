da819c3ef869220c7f5a992056f2d6522dcc9d4c
rn-santos227 <rn_santos227@yahoo.com>
2025-12-28 18:53:34 +0800

    updated setup function in accommodation controller test file

--- src/test/java/com/mgnt/events/controllers/AccommodationControllerTest.java
+++ src/test/java/com/mgnt/events/controllers/AccommodationControllerTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.mgnt.events.services.AccommodationService;
+import com.mgnt.events.util.RequestValidators;
 
 @ExtendWith(MockitoExtension.class)
 public class AccommodationControllerTest {
@@ -28,5 +31,11 @@ public class AccommodationControllerTest {
     _objectMapper = new ObjectMapper();
     _objectMapper.registerModule(new JavaTimeModule());
 
+    _mockMvc = MockMvcBuilders
+      .standaloneSetup(_accommodationController)
+      .setMessageConverters(new MappingJackson2HttpMessageConverter(
+        RequestValidators.requireNonNull(_objectMapper, "Object Mapper")
+      ))
+      .build();
   }
 }
