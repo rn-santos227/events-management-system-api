a82f843b5e1e9a1f8164cc7d9556041b1b5c1981
rn-santos227 <rnsantos227@gmail.com>
2026-01-03 23:09:21 +0800

    updated routes constant file

--- src/main/java/com/mgnt/events/constants/Routes.java
+++ src/main/java/com/mgnt/events/constants/Routes.java
@@ -6,6 +6,11 @@ public class Routes {
   public static final String API_BASE = "/api";
   public static final String API_VERSION = "/v1";
 
+  public static final String APPEND_ID = "/{id}";
+  public static final String APPEND_USER_ID = "/users" + APPEND_ID;
+  public static final String DOWNLOAD = APPEND_ID + "/download";
+  public static final String SEARCH = "/search";
+
   public static final String BASE_PATH = API_BASE + API_VERSION;
   public static final String TEST = BASE_PATH + "/test";
   public static final String AUTH = BASE_PATH + "/auth";
@@ -22,12 +27,8 @@ public class Routes {
   public static final String PRIVILEGES = BASE_PATH + "/privileges";
   public static final String ROLES = BASE_PATH + "/roles";
   public static final String USERS = BASE_PATH + "/users";
+  public static final String USER_SETTINGS = USERS + APPEND_ID + "/settings";
   public static final String AUDIT_LOGS = BASE_PATH + "/audit-logs";
   public static final String VEHICLES = BASE_PATH + "/vehicles";
   public static final String VENUES = BASE_PATH + "/venues";
-
-  public static final String APPEND_ID = "/{id}";
-  public static final String APPEND_USER_ID = "/users" + APPEND_ID;
-  public static final String DOWNLOAD = APPEND_ID + "/download";
-  public static final String SEARCH = "/search";
 }
