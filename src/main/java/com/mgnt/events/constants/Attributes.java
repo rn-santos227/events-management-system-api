5de7b301d28f195e52e9ef0d4adb07404b235c71
rn-santos227 <rnsantos227@gmail.com>
2026-01-03 23:02:07 +0800

    updated attirbutes constant file

--- src/main/java/com/mgnt/events/constants/Attributes.java
+++ src/main/java/com/mgnt/events/constants/Attributes.java
@@ -15,11 +15,15 @@ public class Attributes {
   public static final String CONTACT_NUMBER = "contact_number";
   public static final String CONTACT_PERSON = "contact_person";
   public static final String CONTENT_TYPE = "content_type";
+  public static final String DEFAULT_PAGE_SIZE = "default_page_size";
+  public static final String DENSITY = "density";
   public static final String DESCRIPTION = "description";
   public static final String EMAIL = "email";
+  public static final String EXPIRES_AT = "expires_at";
   public static final String FILE_NAME = "file_name";
   public static final String FILE_SIZE = "file_size";
   public static final String FIRST_NAME = "first_name";
+  public static final String FONT_SIZE = "font_size";
   public static final String FULL_NANE = "full_name";
   public static final String IP_ADDRESS = "ip_address";
   public static final String LAST_NAME = "last_name";
@@ -32,14 +36,15 @@ public class Attributes {
   public static final String STATUS = "status";
   public static final String PATH = "path";
   public static final String PLATE_NUMBER = "plate_number";
+  public static final String REMEMBER_STATE = "remember_state";
   public static final String ROLE = "role";
   public static final String STATUS_CODE = "status_code";
   public static final String STORAGE_KEY = "storage_key";
+  public static final String THEME = "theme";
+  public static final String TOKEN_TYPE = "token_type";
   public static final String TYPE = "type";
   public static final String FUNCTION = "function";
   public static final String URL = "url";
-  public static final String TOKEN_TYPE = "token_type";
-  public static final String EXPIRES_AT = "expires_at";
 
   public static final String CREATED_AT = "created_at";
   public static final String DELETED_AT = "deleted_at";
