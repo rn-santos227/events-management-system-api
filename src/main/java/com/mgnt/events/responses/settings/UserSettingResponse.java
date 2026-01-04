a9e267c953f6121018ba4113dbdeddfb378c23c4
rn-santos227 <rnsantos227@gmail.com>
2026-01-04 19:32:16 +0800

    updated user setting response file

--- src/main/java/com/mgnt/events/responses/settings/UserSettingResponse.java
+++ src/main/java/com/mgnt/events/responses/settings/UserSettingResponse.java
@@ -1,5 +1,12 @@
 package com.mgnt.events.responses.settings;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.mgnt.events.enums.DensityOption;
+import com.mgnt.events.enums.FontSizeOption;
+import com.mgnt.events.enums.ThemeOption;
+
 public record UserSettingResponse(
 
 ) {}
