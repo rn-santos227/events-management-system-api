34d6c7d80f5f58bc131c1691dcf02ca85bac959a
rn-santos227 <rnsantos227@gmail.com>
2026-01-04 19:18:53 +0800

    updated user setting model file

--- src/main/java/com/mgnt/events/models/UserSetting.java
+++ src/main/java/com/mgnt/events/models/UserSetting.java
@@ -1,8 +1,20 @@
 package com.mgnt.events.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import org.hibernate.annotations.SQLDelete;
+
+import com.mgnt.events.constants.Queries;
+import com.mgnt.events.constants.Tables;
 
 @Entity
+@Table(name = Tables.USER_SETTINGS)
+@SQLDelete(sql = Queries.DELETE_USER_SETTINGS)
+@Getter
+@Setter
 public class UserSetting {
   
 }
