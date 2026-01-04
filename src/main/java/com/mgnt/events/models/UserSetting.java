f3bb395a8da079bcfd617cffbad65a4a4f9f5740
rn-santos227 <rnsantos227@gmail.com>
2026-01-04 19:20:42 +0800

    added id in user setting model file

--- src/main/java/com/mgnt/events/models/UserSetting.java
+++ src/main/java/com/mgnt/events/models/UserSetting.java
@@ -1,10 +1,13 @@
 package com.mgnt.events.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
-
 import org.hibernate.annotations.SQLDelete;
 
 import com.mgnt.events.constants.Queries;
@@ -16,5 +19,7 @@ import com.mgnt.events.constants.Tables;
 @Getter
 @Setter
 public class UserSetting {
-  
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
 }
