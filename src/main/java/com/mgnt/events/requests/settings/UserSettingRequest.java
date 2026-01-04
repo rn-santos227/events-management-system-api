e3b4b44e6a6f869f8e6b8bd5483ccc9fb82abea0
rn-santos227 <rnsantos227@gmail.com>
2026-01-04 19:29:46 +0800

    created user setting request file

---
+++ src/main/java/com/mgnt/events/requests/settings/UserSettingRequest.java
@@ -0,0 +1,12 @@
+package com.mgnt.events.requests.settings;
+
+import com.mgnt.events.enums.DensityOption;
+import com.mgnt.events.enums.FontSizeOption;
+import com.mgnt.events.enums.ThemeOption;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record  UserSettingRequest(
+
+) {} 
