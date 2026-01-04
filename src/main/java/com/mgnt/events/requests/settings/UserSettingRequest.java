b44bfbb394b19672ae5097d5fdd6329e20e24ac7
rn-santos227 <rnsantos227@gmail.com>
2026-01-04 19:30:39 +0800

    updated user setting request file

--- src/main/java/com/mgnt/events/requests/settings/UserSettingRequest.java
+++ src/main/java/com/mgnt/events/requests/settings/UserSettingRequest.java
@@ -11,5 +11,12 @@ public record  UserSettingRequest(
   @NotNull(message = "Theme is required")
   ThemeOption theme,
   @NotNull(message = "Density is required")
-  DensityOption density
+  DensityOption density,
+  @NotNull(message = "Font size is required")
+  FontSizeOption fontSize,
+  @NotNull(message = "Default page size is required")
+  @Min(value = 1, message = "Default page size must be at least 1")
+  Integer defaultPageSize,
+  @NotNull(message = "Remember state is required")
+  Boolean rememberState
 ) {} 
