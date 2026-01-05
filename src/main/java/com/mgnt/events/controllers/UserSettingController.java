4883ac842ad9b9b259d219a0e77a95d4c4e0dc8c
rn-santos227 <rnsantos227@gmail.com>
2026-01-05 21:03:39 +0800

    added constructor in user setting controller file

--- src/main/java/com/mgnt/events/controllers/UserSettingController.java
+++ src/main/java/com/mgnt/events/controllers/UserSettingController.java
@@ -4,9 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mgnt.events.constants.Routes;
+import com.mgnt.events.services.UserSettingService;
 
 @RestController
 @RequestMapping(Routes.USER_SETTINGS)
 public class UserSettingController {
-  
+  private final UserSettingService _userSettingService;
+
+  public UserSettingController(UserSettingService userSettingService) {
+    this._userSettingService = userSettingService;
+  }
+
 }
