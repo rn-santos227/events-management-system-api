ae5e23e5428b9484acaf3fcea43e0ce775801bfd
rn-santos227 <rnsantos227@gmail.com>
2026-01-05 21:03:11 +0800

    created user setting controller file

---
+++ src/main/java/com/mgnt/events/controllers/UserSettingController.java
@@ -0,0 +1,12 @@
+package com.mgnt.events.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mgnt.events.constants.Routes;
+
+@RestController
+@RequestMapping(Routes.USER_SETTINGS)
+public class UserSettingController {
+  
+}
