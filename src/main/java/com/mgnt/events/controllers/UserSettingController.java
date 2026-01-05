ff5c967382ffdf31b93b359d3a4b602363fa9a5d
rn-santos227 <rnsantos227@gmail.com>
2026-01-05 21:05:17 +0800

    added get by user id function in user setting controller file

--- src/main/java/com/mgnt/events/controllers/UserSettingController.java
+++ src/main/java/com/mgnt/events/controllers/UserSettingController.java
@@ -1,9 +1,16 @@
 package com.mgnt.events.controllers;
 
+import java.util.UUID;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mgnt.events.constants.Routes;
+import com.mgnt.events.responses.settings.UserSettingResponse;
 import com.mgnt.events.services.UserSettingService;
 
 @RestController
@@ -15,4 +22,8 @@ public class UserSettingController {
     this._userSettingService = userSettingService;
   }
 
+  @GetMapping
+  public UserSettingResponse getByUserId(@PathVariable @NonNull UUID id) {
+    return _userSettingService.getByUserId(id);
+  }
 }
