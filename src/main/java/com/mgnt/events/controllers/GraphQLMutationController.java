e54c750ee10d7be4a705a4fa331d707ce3714be7
rn-santos227 <rnsantos227@gmail.com>
2026-01-08 18:43:46 +0800

    updated graphql mutation controller file

--- src/main/java/com/mgnt/events/controllers/GraphQLMutationController.java
+++ src/main/java/com/mgnt/events/controllers/GraphQLMutationController.java
@@ -2,7 +2,47 @@ package com.mgnt.events.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import com.mgnt.events.services.AccommodationService;
+import com.mgnt.events.services.CategoryService;
+import com.mgnt.events.services.PersonnelService;
+import com.mgnt.events.services.PrivilegeService;
+import com.mgnt.events.services.RoleService;
+import com.mgnt.events.services.UserSettingService;
+import com.mgnt.events.services.UserService;
+import com.mgnt.events.services.VehicleService;
+import com.mgnt.events.services.VenueService;
+
 @Controller
 public class GraphQLMutationController {
-  
+  private final AccommodationService _accommodationService;
+  private final CategoryService _categoryService;
+  private final PersonnelService _personnelService;
+  private final PrivilegeService _privilegeService;
+  private final RoleService _roleService;
+  private final UserSettingService _userSettingService;
+  private final UserService _userService;
+  private final VehicleService _vehicleService;
+  private final VenueService _venueService;
+
+  public GraphQLMutationController(
+    AccommodationService accommodationService,
+    CategoryService categoryService,
+    PersonnelService personnelService,
+    PrivilegeService privilegeService,
+    RoleService roleService,
+    UserSettingService userSettingService,
+    UserService userService,
+    VehicleService vehicleService,
+    VenueService venueService
+  ) {
+    this._accommodationService = accommodationService;
+    this._categoryService = categoryService;
+    this._personnelService = personnelService;
+    this._privilegeService = privilegeService;
+    this._roleService = roleService;
+    this._userSettingService = userSettingService;
+    this._userService = userService;
+    this._vehicleService = vehicleService;
+    this._venueService = venueService;
+  }
 }
